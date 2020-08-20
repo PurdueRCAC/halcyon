9733268e65517932c501bbdc99e9cf67a5066ecb
Shawn Rice <rices@purdue.edu>
2020-08-20 11:03:59 -0400

    fix(theme): Add base URL to header for JS

--- app/Themes/Admin/views/layouts/master.blade.php
+++ app/Themes/Admin/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<meta name="base-url" content="{{ rtrim(url('/'), '/') }}">
 		<meta name="api-token" content="{{ (Auth::user() ? Auth::user()->api_token : '') }}">
 
 		<!-- Styles -->
