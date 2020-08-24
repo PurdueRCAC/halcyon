3f6951abf675d364aa798929e18b2faa220ba2c3
Shawn Rice <rices@purdue.edu>
2020-08-24 09:20:16 -0400

    feat: Add Loggable trait for logging requests

---
+++ app/Modules/History/Traits/Loggable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Modules\History\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Modules\History\Models\History;
+
+trait Loggable
+{
+	/**
+	 * boot method
+	 *
+	 * @return  null
+	 */
+	protected function log($app, $method = 'GET', $status = 200, $payload = array(), $uri = '')
+	{
+		$method = strtoupper($method);
+
+		if (!in_array($method, ['GET', 'POST', 'PUT', 'PATCH', 'HEAD', 'DELETE']))
+		{
+			$method = 'GET';
+		}
+
+		Log::create([
+			'ip'              => request()->ip(),
+			'user'            => (auth()->user() ? auth()->user()->id : 0),
+			'status'          => (int)$status,
+			'transportmethod' => $method,
+			'servername'      => request()->getHttpHost(),
+			'uri'             => $uri,
+			'app'             => $app,
+			'payload'         => json_encode($payload),
+		]);
+	}
+}
