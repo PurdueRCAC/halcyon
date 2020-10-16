82183a00d5e8f33d288e152d1cd33518de4733db
Shawn Rice <rices@purdue.edu>
2020-10-16 13:43:48 -0400

    feat(rcac theme): Add email templates

---
+++ app/Themes/Rcac/views/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+	<td class="header">
+		<a href="{{ $url }}">
+			<img src="{{ asset('themes/Rcac/images/RCAC_SIG_Logo_RGB__PU-H-Full-RGB_Black_white.png') }}" width="395" height="35" class="logo" alt="{{ $slot }}">
+		</a>
+	</td>
+</tr>
