122caa728cfc471466be3904d163d36892b4c38d
Shawn Rice <rices@purdue.edu>
2020-10-20 09:13:56 -0400

    feat(queues): Add various emails to cron scheduling

--- app/Modules/Queues/Providers/ScheduleServiceProvider.php
+++ app/Modules/Queues/Providers/ScheduleServiceProvider.php
@@ -8,6 +8,10 @@ use App\Modules\Queues\Console\EmailQueueAuthorizedCommand;
 use App\Modules\Queues\Console\EmailQueueDeniedCommand;
 use App\Modules\Queues\Console\EmailQueueRemovedCommand;
 use App\Modules\Queues\Console\EmailQueueRequestedCommand;
+use App\Modules\Queues\Console\EmailFreeAuthorizedCommand;
+use App\Modules\Queues\Console\EmailFreeDeniedCommand;
+use App\Modules\Queues\Console\EmailFreeRemovedCommand;
+use App\Modules\Queues\Console\EmailFreeRequestedCommand;
 use App\Modules\Queues\Console\EmailWelcomeClusterCommand;
 use App\Modules\Queues\Console\EmailWelcomeFreeCommand;
 
@@ -20,17 +24,20 @@ class ScheduleServiceProvider extends ServiceProvider
 			$schedule = $this->app->make(Schedule::class);
 
 			$schedule->command(EmailQueueAuthorizedCommand::class)->cron(config('module.queues.schedule.emailqueueauthorized', '*/10 * * * *'));
+			$schedule->command(EmailFreeAuthorizedCommand::class)->cron(config('module.queues.schedule.emailfreeauthorized', '*/20 * * * *'));
 
 			$schedule->command(EmailQueueDeniedCommand::class)->cron(config('module.queues.schedule.emailqueuedenied', '*/20 * * * *'));
+			$schedule->command(EmailFreeDeniedCommand::class)->cron(config('module.queues.schedule.emailfreedenied', '*/20 * * * *'));
 
 			$schedule->command(EmailQueueRemovedCommand::class)->cron(config('module.queues.schedule.emailqueueremoved', '*/10 * * * *'));
+			$schedule->command(EmailFreeRemovedCommand::class)->cron(config('module.queues.schedule.emailfreeremoved', '*/20 * * * *'));
 
 			$schedule->command(EmailQueueRequestedCommand::class)->cron(config('module.queues.schedule.emailqueuerequested', '*/20 * * * *'));
+			$schedule->command(EmailFreeRequestedCommand::class)->cron(config('module.queues.schedule.emailfreerequested', '*/20 * * * *'));
 
 			$schedule->command(EmailWelcomeClusterCommand::class)->cron(config('module.queues.schedule.emailwelcomecluster', '0 5 * * * '));
-
 			$schedule->command(EmailWelcomeFreeCommand::class)->cron(config('module.queues.schedule.emailwelcomefree', '0 5 * * * '));
-			//$schedule->command(EmailQueueRemovedExpiredCommand::class)->cron(config('module.queues.schedule.emailqueueremovedexpired', '*/30 * * * *'));
+			//$schedule->command(EmailWelcomeItarCommand::class)->cron(config('module.queues.schedule.emailwelcomeitar', '0 * * * * '));
 		});
 	}
 }
