f342c30405b838f9baf94307611989a4613f5439
Shawn Rice <rices@purdue.edu>
2020-11-16 09:58:34 -0500

    fix(knowledge): Fix snippets that have no parent

--- app/Modules/Knowledge/Resources/views/admin/snippets/edit.blade.php
+++ app/Modules/Knowledge/Resources/views/admin/snippets/edit.blade.php
@@ -81,7 +81,7 @@ app('pathway')
 				<div class="form-group">
 					<label for="field-parent_id">{{ trans('knowledge::knowledge.parent') }}:</label>
 					<select name="fields[parent_id]" id="field-parent_id" class="form-control searchable-select">
-						<!--<option value="0">{{ trans('global.none') }}</option>-->
+						<option value="1">{{ trans('global.none') }}</option>
 						<?php foreach ($tree as $pa): ?>
 							<?php $selected = ($pa->id == $row->parent_id ? ' selected="selected"' : ''); ?>
 							<option value="{{ $pa->id }}"<?php echo $selected; ?> data-path="/{{ $pa->path }}"><?php echo str_repeat('|&mdash; ', $pa->level - 1) . e(Illuminate\Support\Str::limit($pa->title, 70)); ?></option>
