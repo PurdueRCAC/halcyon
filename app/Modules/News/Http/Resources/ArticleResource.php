3afe3d6fec907388c7a3773a538031f7db839615
Shawn Rice <rices@purdue.edu>
2020-11-17 16:08:09 -0500

    style(news): Remove extra whitespace

--- app/Modules/News/Http/Resources/ArticleResource.php
+++ app/Modules/News/Http/Resources/ArticleResource.php
@@ -44,7 +44,6 @@ class ArticleResource extends JsonResource
 			{
 				$res->name = $associated->name;
 			}
-			
 		});
 
 		//$this->username = $this->creator ? $this->creator->name : trans('global.unknown');
