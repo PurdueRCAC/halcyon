aadba9949265ec230f889221436e44d329b17aae
Shawn Rice <rices@purdue.edu>
2020-12-04 13:11:59 -0500

    fix: Add unix groups to user history

--- app/Modules/History/Resources/views/site/profile.blade.php
+++ app/Modules/History/Resources/views/site/profile.blade.php
@@ -29,8 +29,52 @@
 								{{ $group->datecreated->format('M d, Y') }}
 							</td>
 							<td>
-								@if ($group->dateremoved && $group->dateremoved != '0000-00-00 00:00:00' && $group->dateremoved != '-0001-11-30 00:00:00')
+								@if ($group->isTrashed())
 									{{ $group->dateremoved->format('M d, Y') }}
+								@else
+									-
+								@endif
+							</td>
+						</tr>
+						<?php
+					}
+					?>
+				</tbody>
+			</table>
+		@else
+			<p class="alert alert-info">No history found.</p>
+		@endif
+
+		@if (count($groups) > 0)
+			<table class="table table-hover">
+				<caption>Unix Group History</caption>
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">Group</th>
+						<th scope="col">Unix Group</th>
+						<th scope="col">Added</th>
+						<th scope="col">Removed</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					foreach ($unixgroups as $group)
+					{
+						$ug = $group->unixgroup()->withTrashed()->first();
+						?>
+						<tr id="unixgroup{{ $group->id }}">
+							<td>
+								{{ $ug->group ? $ug->group->name : trans('global.unknown') }}
+							</td>
+							<td>
+								{{ $ug ? $ug->longname : trans('global.unknown') }}
+							</td>
+							<td>
+								{{ $group->datetimecreated->format('M d, Y') }}
+							</td>
+							<td>
+								@if ($group->isTrashed())
+									{{ $group->datetimeremoved->format('M d, Y') }}
 								@else
 									-
 								@endif
@@ -58,81 +102,33 @@
 				</thead>
 				<tbody>
 					<?php
-					/*if (count($user->priorownerofgroups))
-					{
-						foreach ($user->priorownerofgroups as $own)
-						{
-							$group = $ws->get($own['group']);
-							if ($own['group'] != ROOT_URI . "group/0")
-							{
-								foreach ($group->queues as $q)
-								{
-									$qq = $ws->get($q['id']);
-									$created_date = date('M d, Y', strtotime($own['created']));
-									$removed_date = date('M d, Y', strtotime($own['removed']));
-
-									$qid = preg_replace('/^' . str_replace('/', '\/', ROOT_URI) . 'queue\//', '', $q['id'], 1, $count);
-									?>
-									<tr>
-										<?php if ($qq->group == ROOT_URI . "group/0") { ?>
-											<td><?php echo $q['resource']['name']; ?></td>
-										<?php } else { ?>
-											<td><?php echo $q['subresource']['name']; ?></td>
-										<?php } ?>
-										<td>
-											<a href="/admin/queue/edit/?q=<?php echo $qid; ?>"><?php echo $q['name']; ?></a>
-										</td>
-										<td><?php echo $created_date; ?></td>
-										<td><?php echo $removed_date; ?></td>
-									</tr>
-									<?php
-								}
-							}
-						}
-					}
-
-					if (count($user->priormemberofqueues) > 0)
-					{
-						foreach ($user->priormemberofqueues as $own)
-						{
-							$q = $ws->get($own['queue']);
-							$created_date = date('M d, Y', strtotime($own['created']));
-							$removed_date = date('M d, Y', strtotime($own['removed']));
-
-							$qid = preg_replace('/^' . str_replace('/', '\/', ROOT_URI) . 'queue\//', '', $q->id, 1, $count);
-							?>
-							<tr>
-								<?php if ($q->group == ROOT_URI . "group/0") { ?>
-									<td><?php echo $q->resource['name']; ?></td>
-								<?php } else { ?>
-									<td><?php echo $q->subresource['name']; ?></td>
-								<?php } ?>
-								<td><a href="/admin/queue/edit/?q=<?php echo $qid; ?>"><?php echo $q->name; ?></a></td>
-								<td><?php echo $created_date; ?></td>
-								<td><?php echo $removed_date; ?></td>
-							</tr>
-							<?php
-						}
-					}*/
-
 					foreach ($queues as $queue)
 					{
+						$q = $queue->queue()->withTrashed()->first();
 						?>
-						<tr>
+						<tr id="queueuser{{ $queue->id }}">
 							<td>
-								{{ $queue->type ? $queue->type->name : trans('global.unknown') }}
+								{{ $q->resource ? $q->resource->name : trans('global.unknown') }}
 							</td>
 							<td>
-								{{ $queue->queue ? $queue->queue->name : trans('global.unknown') }}
+								{{ $q ? $q->name : trans('global.unknown') }}
 							</td>
 							<td>
 								{{ $queue->datetimecreated->format('M d, Y') }}
 							</td>
 							<td>
-								@if ($queue->datetimeremoved && $queue->datetimeremoved != '0000-00-00 00:00:00' && $queue->datetimeremoved != '-0001-11-30 00:00:00')
+								@if ($queue->isTrashed())
 									{{ $queue->datetimeremoved->format('M d, Y') }}
 								@else
-									-
+									@if ($q->isTrashed())
+										{{ $q->datetimeremoved->format('M d, Y') }}
+									@else
+										@if ($q->resource->isTrashed())
+											{{ $q->resource->datetimeremoved->format('M d, Y') }}
+										@else
+											-
+										@endif
+									@endif
 								@endif
 							</td>
 						</tr>
