6395417f15fefe3d7033c3cd23f63cb66ebc2c80
Shawn Rice <rices@purdue.edu>
2020-12-04 13:12:28 -0500

    feat: Add helper to check if trashed state or not

--- app/Modules/Queues/Models/Scheduler.php
+++ app/Modules/Queues/Models/Scheduler.php
@@ -89,6 +89,16 @@ class Scheduler extends Model
 	];
 
 	/**
+	 * Determine if in a trashed state
+	 *
+	 * @return  bool
+	 */
+	public function isTrashed()
+	{
+		return ($this->datetimeremoved && $this->datetimeremoved != '0000-00-00 00:00:00' && $this->datetimeremoved != '-0001-11-30 00:00:00');
+	}
+
+	/**
 	 * Defines a relationship to queues
 	 *
 	 * @return  object
