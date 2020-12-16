94fc950f504920e888ff2e8f46a54096078179b8
Shawn Rice <rices@purdue.edu>
2020-12-16 08:32:33 -0500

    fix(queues): Add endpoints to results

--- app/Modules/Queues/Http/Resources/QueueResource.php
+++ app/Modules/Queues/Http/Resources/QueueResource.php
@@ -20,47 +20,105 @@ class QueueResource extends JsonResource
 		$now = Carbon::now();
 
 		$data['resource'] = $this->resource()->get()->first();
-		$data['subresource'] = $this->subresource;
-		$data['scheduler_policy'] = $this->schedulerPolicy;
-		$data['scheduler'] = $this->scheduler;
-		$data['sizes'] = $this->sizes;
+		$data['resource']['api'] = route('api.resources.read', ['id' => $data['resource']['id']]);
 
-		$data['loans'] = $this
-			->loans()
+		$data['subresource'] = $this->subresource;
+		$data['subresource']['api'] = route('api.resources.subresources.read', ['id' => $data['subresource']['id']]);
+
+		$data['schedulerpolicy'] = $this->schedulerPolicy;
+		$data['schedulerpolicy']['api'] = route('api.queues.schedulerpolicies.read', ['id' => $data['schedulerpolicy']['id']]);
+
+		$data['scheduler'] = $this->scheduler;
+		$data['scheduler']['api'] = route('api.queues.schedulers.read', ['id' => $data['scheduler']['id']]);
+
+		$data['sizes'] = $this->sizes()
 			->where(function($where) use ($now)
 			{
 				$where->whereNull('datetimestop')
 					->orWhere('datetimestop', '=', '0000-00-00 00:00:00')
 					->orWhere('datetimestop', '>', $now->toDateTimeString());
 			})
-			->get();
+			->get()
+			->each(function($item, $key)
+			{
+				$item->api = route('api.queues.sizes.read', ['id' => $item->id]);
+			});
 
-		$data['priorloans'] = $this
-			->loans()
+		$data['priorsizes'] = $this->sizes()
 			->where(function($where) use ($now)
 			{
 				$where->whereNotNull('datetimestop')
 					->where('datetimestop', '!=', '0000-00-00 00:00:00')
 					->where('datetimestop', '<=', $now->toDateTimeString());
 			})
-			->get();
+			->get()
+			->each(function($item, $key)
+			{
+				$item->api = route('api.queues.sizes.read', ['id' => $item->id]);
+			});
 
-		$data['users'] = $this->users;
-		$data['priorusers'] = $this->users()->onlyTrashed()->where('datetimeremoved', '!=', '0000-00-00 00:00:00')->get();
-		$data['walltimes'] = $this->walltimes;
+		$data['loans'] = $this->loans()
+			->where(function($where) use ($now)
+			{
+				$where->whereNull('datetimestop')
+					->orWhere('datetimestop', '=', '0000-00-00 00:00:00')
+					->orWhere('datetimestop', '>', $now->toDateTimeString());
+			})
+			->get()
+			->each(function($item, $key)
+			{
+				$item->api = route('api.queues.loans.read', ['id' => $item->id]);
+			});
 
-		$data['totalcores'] = $this->totalcores;
-		$data['totalnodes'] = $this->totalnodes;
-		$data['soldcores'] = $this->soldcores;
-		$data['soldnodes'] = $this->soldnodes;
+		$data['priorloans'] = $this->loans()
+			->where(function($where) use ($now)
+			{
+				$where->whereNotNull('datetimestop')
+					->where('datetimestop', '!=', '0000-00-00 00:00:00')
+					->where('datetimestop', '<=', $now->toDateTimeString());
+			})
+			->get()
+			->each(function($item, $key)
+			{
+				$item->api = route('api.queues.loans.read', ['id' => $item->id]);
+			});
+
+		$data['users'] = $this->users()
+			->withTrashed()
+			->whereIsActive()
+			->orderBy('id', 'asc')
+			->get()
+			->each(function($item, $key)
+			{
+				$item->api = route('api.queues.users.read', ['id' => $item->id]);
+			});
+
+		$data['priorusers'] = $this->users()
+			->onlyTrashed()
+			->where('datetimeremoved', '!=', '0000-00-00 00:00:00')
+			->orderBy('id', 'asc')
+			->get()
+			->each(function($item, $key)
+			{
+				$item->api = route('api.queues.users.read', ['id' => $item->id]);
+			});
+
+		$data['walltimes'] = $this->walltimes
+			->each(function($item, $key)
+			{
+				$item->api = route('api.queues.walltimes.read', ['id' => $item->id]);
+			});
+
+		$data['totalcores']  = $this->totalcores;
+		$data['totalnodes']  = $this->totalnodes;
+		$data['soldcores']   = $this->soldcores;
+		$data['soldnodes']   = $this->soldnodes;
 		$data['loanedcores'] = $this->loanedcores;
 		$data['loanednodes'] = $this->loanednodes;
-		$data['active'] = $this->active;
+		$data['active']      = $this->active;
 
 		$data['api'] = route('api.queues.read', ['id' => $this->id]);
-		//$data['url'] = route('site.queues.show', ['id' => $this->id]);
 
-		//$data['canCreate'] = false;
 		$data['can']['edit']   = false;
 		$data['can']['delete'] = false;
 
