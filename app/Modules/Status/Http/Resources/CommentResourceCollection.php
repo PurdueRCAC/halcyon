957d98e1ba9f5d8ce854ef01969ebad402105df6
Shawn Rice <rices@purdue.edu>
2021-01-13 10:38:32 -0500

    fix(status): Remove unused files, update assets

--- app/Modules/Status/Http/Resources/CommentResourceCollection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Modules\Issues\Http\Resources;
-
-use Illuminate\Http\Resources\Json\ResourceCollection;
-
-class CommentResourceCollection extends ResourceCollection
-{
-}
--- app/Modules/Status/Resources/assets/js/site.js
+++ app/Modules/Status/Resources/assets/js/site.js
@@ -15,7 +15,7 @@ var keywords_pending = 0;
  * @param   {string}  comment
  * @return  {void}
  */
-function StatusSave(id, option) {
+function StatusSave(url, id, option) {
 	var img = document.getElementById(id + "_icon");
 
 	//var icon = img.querySelectorAll('.fa');
@@ -28,11 +28,11 @@ function StatusSave(id, option) {
 		'status': option.value
 	};
 	post = JSON.stringify(post);
-	console.log(post);
-	WSPostURL(id, post, function (xml, option) {
+
+	WSPutURL(url, post, function (xml, option) {
 		//var img = document.getElementById(comment + "_commenttextsaveiconimg");
 
-		if (xml.status == 200) {
+		if (xml.status < 400) {
 			var results = JSON.parse(xml.responseText);
 
 			/*if (option.value == 1) {
@@ -63,87 +63,6 @@ function StatusSave(id, option) {
 }
 
 /**
- * Callback after saving edited report text
- *
- * @param   {object}  xml
- * @param   {string}  report
- * @return  {void}
- */
-/*function IssuesSavedReportText(xml, report) {
-	var img = document.getElementById(report + "_textsaveiconimg");
-
-	if (xml.status == 200) {
-		var results = JSON.parse(xml.responseText);
-
-		var icon = document.getElementById(report + "_textsaveicon");
-			icon.onclick = function () {
-				IssuesSaveReportText(report);
-			};
-			icon.style.display = "none";
-		var text = document.getElementById(report + "_text");
-			text.style.display = "block";
-			text.innerHTML = results['formattedreport'];
-		document.getElementById(report + "_textarea").style.display = "none";
-		document.getElementById(report + "_textediticon").style.display = "block";
-		document.getElementById(report + "_textcancelicon").style.display = "none";
-	} else if (xml.status == 403) {
-		img.className = "fa fa-exclamation-circle";
-		img.parentNode.title = "Unable to save changes, grace editing window has passed.";
-	} else {
-		img.className = "fa fa-exclamation-circle";
-		img.parentNode.title = "Unable to save changes, reload the page and try again.";
-	}
-}*/
-
-/**
- * Callback after saving edited comment text
- *
- * @param   {object}  xml
- * @param   {string}  report
- * @return  {void}
- */
-/*function IssuesSavedCommentText(xml, comment) {
-	var img = document.getElementById(comment + "_commenttextsaveiconimg");
-
-	if (xml.status == 200) {
-		var results = JSON.parse(xml.responseText);
-		var panel = document.getElementById("comment" + comment);
-		if (results.resolution == 1) {
-			panel.className = "panel panel-default issue-resolution";
-		} else {
-			panel.className = "panel panel-default";
-		}
-
-		var text = document.getElementById(comment + "_comment");
-			text.style.display = "block";
-			text.innerHTML = results['formattedcomment'];
-
-		var box = document.getElementById(comment + "_commenttextarea");
-			box.style.display = "none";
-
-		var cicon = document.getElementById(comment + "_commenttextcancelicon");
-			cicon.style.display = "none";
-
-		var editicon = document.getElementById(comment + "_commenttextediticon");
-			editicon.style.display = "block";
-
-		var icon = document.getElementById(comment + "_commenttextsaveicon");
-			icon.disabled = false;
-		var img = document.getElementById(comment + "_commenttextsaveiconimg");
-			img.className = "fa fa-save";
-
-		var d = document.getElementById(comment + "_commenttextareacontrols");
-			d.className = "row comment-controls hide";
-	} else if (xml.status == 403) {
-		img.className = "fa fa-exclamation-circle";
-		img.parentNode.title = "Unable to save changes, grace editing window has passed.";
-	} else {
-		img.className = "fa fa-exclamation-circle";
-		img.parentNode.title = "Unable to save changes, reload the page and try again.";
-	}
-}*/
-
-/**
  * Initiate event hooks
  */
 document.addEventListener('DOMContentLoaded', function () {
@@ -155,7 +74,11 @@ document.addEventListener('DOMContentLoaded', function () {
 			statuses[i].addEventListener('change', function (event) {
 				event.preventDefault();
 
-				StatusSave(this.getAttribute('data-id'), this.options[this.selectedIndex]);
+				StatusSave(
+					this.getAttribute('data-api'),
+					this.getAttribute('data-id'),
+					this.options[this.selectedIndex]
+				);
 			});
 		}
 	}
