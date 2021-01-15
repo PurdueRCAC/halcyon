3b4b1cea724bd200a59b54ddca1b86ee81322d96
Shawn Rice <rices@purdue.edu>
2021-01-15 07:52:37 -0500

    feat: Add Grafana/Prometheus listener

---
+++ app/Listeners/Resources/Grafana/Grafana.php
@@ -0,0 +1,150 @@
+<?php
+namespace App\Listeners\Resources\Grafana;
+
+use Illuminate\Support\Facades\Storage;
+use App\Modules\Status\Events\StatusRetrieval;
+use App\Modules\Resources\Entities\Asset;
+use GuzzleHttp\Client;
+use Carbon\Carbon;
+
+/**
+ * Grafana listener for Resources
+ */
+class Grafana
+{
+	/**
+	 * Register the listeners for the subscriber.
+	 *
+	 * @param  Illuminate\Events\Dispatcher  $events
+	 * @return void
+	 */
+	public function subscribe($events)
+	{
+		$events->listen(StatusRetrieval::class, self::class . '@handleStatusRetrieval');
+	}
+
+	/**
+	 * Plugin that loads module positions within content
+	 *
+	 * @param   StatusRetrieval  $event
+	 * @return  void
+	 */
+	public function handleStatusRetrieval(StatusRetrieval $event)
+	{
+		$resource = $event->asset;
+
+		$url = $resource->params->get('monitor');
+		$url = $resource->rolename == 'hammer' ? 'http://grafana.hammer.rcac.purdue.edu:9090' : $url;
+
+		if (!$url)
+		{
+			return;
+		}
+
+		$retrieve = true;
+		$cache = storage_path('app/status_' . $resource->rolename . '.json');
+		$hourago = Carbon::now()->modify('-1 hour')->timestamp;
+
+		if (file_exists($cache))
+		{
+			$lastmodified = Storage::lastModified($cache);
+
+			if ($lastmodified > $hourago)
+			{
+				$retrieve = false;
+
+				$data = json_decode(file_get_contents($path), true);
+				$resource->statusUpdate = Carbon::parse($lastmodified);
+			}
+		}
+
+		if ($retrieve)
+		{
+			try
+			{
+				$client = new Client();
+
+				$checks = [
+					// Frontends
+					'frontends' => '/api/v1/query?query=frontend_status_check_sum&time=1610710613',
+					// Jupyterhub
+					'jupyterhub' => '/api/v1/query?query=jupyterhub_status&time=1610710848',
+					// Thinlinc
+					'thinlinc' => '/api/v1/query?query=thinlinc_status&time=1610710848',
+					// SSH
+					'ssh' => '/api/v1/query?query=sum(frontend_status_check_count)&time=1610710848',
+				];
+
+				$data = array();
+
+				foreach ($checks as $check => $endpoint)
+				{
+					$data[$check] = array();
+
+					$res = $client->request('GET', $url . $endpoint);
+
+					//$status = $res->getStatusCode();
+
+					if ($res->getStatusCode() >= 400)
+					{
+						echo $endpoint . ' - ' . $res->getStatusCode() . '<br />';
+						continue;
+					}
+
+					$response = json_decode($res->getBody()->getContents());
+
+					// Endpoint result example:
+					// {
+					//   "status": "success",
+					//   "data": {
+					//     "resultType": "vector",
+					//     "result": [
+					//       {
+					//         "metric": {},
+					//         "value": [
+					//           1610710848,
+					//           "0"
+					//         ]
+					//       }
+					//     ]
+					//   }
+					// }
+					if ($response->status == 'success')
+					{
+						$data[$check] = $response->data->result;
+						continue;
+						/*foreach ($response->data->result as $res)
+						{
+							$metrics = array();
+
+							if (isset($res->metric))
+							{
+								$metrics = $res->metric;
+							}
+
+							foreach ($res->value as $value)
+							{
+								$data[$check][] = array(
+									'name' => $check,
+									'value' => $value[1],
+									'metrics' => $metrics,
+								);
+							}
+						}*/
+					}
+				}
+
+				$resource->statusUpdate = Carbon::now();
+
+				//Storage::put($cache, json_encode($data));
+			}
+			catch (\Exception $e)
+			{
+			}
+		}
+		//echo '<pre>';print_r($data);echo '</pre>';
+		$resource->data = $data;
+
+		$event->asset = $resource;
+	}
+}
---
+++ app/Listeners/Resources/Grafana/lang/en/grafana.php
@@ -0,0 +1,5 @@
+<?php
+return [
+	'listener name' => 'Resources - Grafana',
+	'listener desc' => 'This retrieves data from a Grafana API for a specified resource.',
+];
