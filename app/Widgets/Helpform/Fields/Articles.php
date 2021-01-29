bc724738e272d4e85900f0d8404182e5cc4d4f75
Shawn Rice <rices@purdue.edu>
2021-01-29 15:15:35 -0500

    fix: Fix undeclared var

--- app/Widgets/Helpform/Fields/Articles.php
+++ app/Widgets/Helpform/Fields/Articles.php
@@ -35,30 +35,29 @@ class Articles extends Select
 
 		$options = $this->getOptions();
 
-		if (!$topic)
+		$k = 0;
+		if (!empty($topic))
 		{
-			$topic = array();
-		}
-		foreach ($topic as $k => $t)
-		{
-			$html[] = '<div class="topic-group">';
-			$html[] = '<div class="form-group"><hr />';
-				$html[] = '<label for="' . $this->id . '_' . $k . '_title">' . trans('widget.helpform::helpform.title') . '</label>';
-				$html[] = '<input type="text" name="'.$this->name.'[' . $k . '][title]" id="' . $this->id . '_' . $k . '_title" value="' . htmlspecialchars($t['title'], ENT_COMPAT, 'UTF-8') . '"' . $size . $class . ' />';
-			$html[] = '</div>';
+			foreach ($topic as $k => $t)
+			{
+				$html[] = '<div class="topic-group">';
+				$html[] = '<div class="form-group"><hr />';
+					$html[] = '<label for="' . $this->id . '_' . $k . '_title">' . trans('widget.helpform::helpform.title') . '</label>';
+					$html[] = '<input type="text" name="'.$this->name.'[' . $k . '][title]" id="' . $this->id . '_' . $k . '_title" value="' . htmlspecialchars($t['title'], ENT_COMPAT, 'UTF-8') . '"' . $size . $class . ' />';
+				$html[] = '</div>';
 
-			$html[] = '<div class="form-group">';
-				$html[] = '<label for="' . $this->id . '_' . $k . '_article">' . trans('widget.helpform::helpform.article') . '</label>';
-				$html[] = '<select name="'.$this->name.'[' . $k . '][article]" id="' . $this->id . '_' . $k . '_article"' . $size . $class . ' />';
-				foreach ($options as $option)
-				{
-					$html[] = '<option value="' . $option['value'] . '"' . ($t['article'] == $option['value'] ? ' selected="selected"' : '') . '>' . $option['text'] . '</option>';
-				}
-				$html[] = '</select>';
-			$html[] = '</div>';
-			$html[] = '</div>';
+				$html[] = '<div class="form-group">';
+					$html[] = '<label for="' . $this->id . '_' . $k . '_article">' . trans('widget.helpform::helpform.article') . '</label>';
+					$html[] = '<select name="'.$this->name.'[' . $k . '][article]" id="' . $this->id . '_' . $k . '_article"' . $size . $class . ' />';
+					foreach ($options as $option)
+					{
+						$html[] = '<option value="' . $option['value'] . '"' . ($t['article'] == $option['value'] ? ' selected="selected"' : '') . '>' . $option['text'] . '</option>';
+					}
+					$html[] = '</select>';
+				$html[] = '</div>';
+				$html[] = '</div>';
+			}
 		}
-
 		$k++;
 
 		$html[] = '<div class="topic-group"><hr />';
