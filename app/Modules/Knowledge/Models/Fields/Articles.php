88f3f628b5f63b0f09ea028bf8c1d81616f0c7f4
Shawn Rice <rices@purdue.edu>
2021-02-16 08:19:33 -0500

    feat(knowledge): Add param field for selecting page

---
+++ app/Modules/Knowledge/Models/Fields/Articles.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Modules\Knowledge\Models\Fields;
+
+use Illuminate\Support\Str;
+use App\Halcyon\Form\Fields\Select;
+use App\Modules\Knowledge\Models\Page;
+
+/**
+ * Supports an article picker.
+ */
+class Articles extends Select
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var  string
+	 */
+	protected $type = 'Articles';
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 */
+	protected function getOptions()
+	{
+		$rows = Page::tree();
+
+		$options = array();
+
+		$options[] = array(
+			'value' => 0,
+			'text' => trans('knowledge::knowledge.select page')
+		);
+
+		foreach ($rows as $row)
+		{
+			$options[] = array(
+				'value' => $row->id,
+				'text' => str_repeat('|&mdash; ', $row->level) . e(Str::limit($row->title, 70))
+			);
+		}
+
+		return $options;
+	}
+}
