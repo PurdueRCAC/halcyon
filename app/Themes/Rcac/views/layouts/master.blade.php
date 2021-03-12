0dc97e3d90b7038172cb244fa05e44ba05f1dc58
Shawn Rice <rices@purdue.edu>
2021-03-12 15:09:02 -0500

    feat(rcac): Allow for specifying attributes for link tag

--- app/Themes/Rcac/views/layouts/master.blade.php
+++ app/Themes/Rcac/views/layouts/master.blade.php
@@ -37,19 +37,22 @@
 		<?php
 		$styles = array(
 			//'themes/rcac/css/jquerytimepicker_min.css',
-			'themes/rcac/css/font-awesome-css.min.css',
-			'themes/rcac/js/common/jquery-ui-1.12.1/themes/base/jquery-ui.min.css',
-			'themes/rcac/css/college.css',
-			'themes/rcac/css/content.css',
+			//'https://use.typekit.net/ghc8hdz.css' => 'rel="preload" as="style"',
+			'themes/rcac/css/font-awesome-css.min.css' => 'rel="stylesheet" type="text/css" media="all"',
+			'themes/rcac/js/common/jquery-ui-1.12.1/themes/base/jquery-ui.min.css' => 'rel="stylesheet" type="text/css" media="all"',
+			'themes/rcac/css/college.css' => 'rel="stylesheet" type="text/css" media="all"',
+			'themes/rcac/css/content.css' => 'rel="stylesheet" type="text/css" media="all"'
 			//'themes/rcac/css/site.css',
 		);
 		if (!app('isAdmin') && Auth::check()):
-			$styles[] = 'themes/rcac/css/admin.css';
+			$styles['themes/rcac/css/admin.css'] = 'rel="stylesheet" type="text/css" media="all"';
 		endif;
 
-		foreach ($styles as $css):
+		foreach ($styles as $css => $attrs):
+			$attrs = $attrs ?: 'rel="stylesheet" media="all"';
+			$sfx = (substr($css, 0, 4) != 'http' ? '?v=' . filemtime(public_path() . '/' . $css) : '');
 			?>
-		<link rel="stylesheet" type="text/css" media="all" href="{{ asset($css . '?v=' . filemtime(public_path() . '/' . $css)) }}" />
+		<link {!! $attrs !!} href="{{ asset($css . $sfx) }}" />
 <?php
 		endforeach;
 		?>
