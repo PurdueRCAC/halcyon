da6976630f2c8b4673cee315419ce542dac72bf3
Shawn Rice <rices@purdue.edu>
2021-04-09 10:17:54 -0400

    fix(core): Minor style adjustments

--- app/Modules/Core/Resources/views/admin/info/index.blade.php
+++ app/Modules/Core/Resources/views/admin/info/index.blade.php
@@ -25,57 +25,47 @@ app('pathway')
 
 @section('content')
 <form action="{{ route('admin.core.sysinfo') }}" method="post" name="adminForm" id="adminForm">
-	<div class="card">
-		<div class="tabs" id="config-document">
-			<ul>
-				<li><a href="#page-site">{{ trans('core::info.system information') }}</a></li>
-				<li><a href="#page-phpsettings">{{ trans('core::info.php settings') }}</a></li>
-				<?php /*<li><a href="#page-config">{{ trans('core::info.configuration') }}</a></li>*/ ?>
-				<li><a href="#page-directory">{{ trans('core::info.directory permissions') }}</a></li>
-				<li><a href="#page-phpinfo">{{ trans('core::info.php info') }}</a></li>
-			</ul>
 
-			<div id="page-site" class="tab">
-				<div class="row noshow">
-					<div class="col-md-12">
-						@include('core::admin.info.system', ['info' => $model->getInfo()])
-					</div>
-				</div>
+	<div class="tabs" id="config-document">
+		<ul>
+			<li><a href="#page-site">{{ trans('core::info.system information') }}</a></li>
+			<li><a href="#page-phpsettings">{{ trans('core::info.php settings') }}</a></li>
+			<?php /*<li><a href="#page-config">{{ trans('core::info.configuration') }}</a></li>*/ ?>
+			<li><a href="#page-directory">{{ trans('core::info.directory permissions') }}</a></li>
+			<li><a href="#page-phpinfo">{{ trans('core::info.php info') }}</a></li>
+		</ul>
+
+		<div id="page-site" class="tab">
+			<div class="card">
+				@include('core::admin.info.system', ['info' => $model->getInfo()])
 			</div>
+		</div>
 
-			<div id="page-phpsettings" class="tab">
-				<div class="row noshow">
-					<div class="col-md-12">
-						@include('core::admin.info.phpsettings', ['info' => $model->getPhpSettings()])
-					</div>
-				</div>
+		<div id="page-phpsettings" class="tab">
+			<div class="card">
+				@include('core::admin.info.phpsettings', ['info' => $model->getPhpSettings()])
 			</div>
+		</div>
 
-			<?php /*<div id="page-config" class="tab">
-				<div class="row noshow">
-					<div class="col-md-12">
-						@include('core::admin.info.config', ['info' => $model->getConfig()])
-					</div>
-				</div>
-			</div>*/ ?>
-
-			<div id="page-directory" class="tab">
-				<div class="row noshow">
-					<div class="col-md-12">
-						@include('core::admin.info.directory', ['info' => $model->getDirectory()])
-					</div>
-				</div>
+		<?php /*<div id="page-config" class="tab">
+			<div class="card">
+				@include('core::admin.info.config', ['info' => $model->getConfig()])
 			</div>
+		</div>*/ ?>
 
-			<div id="page-phpinfo" class="tab">
-				<div class="row noshow">
-					<div class="col-md-12">
-						@include('core::admin.info.phpinfo', ['info' => $model->getPhpInfo()])
-					</div>
-				</div>
+		<div id="page-directory" class="tab">
+			<div class="card">
+				@include('core::admin.info.directory', ['info' => $model->getDirectory()])
+			</div>
+		</div>
+
+		<div id="page-phpinfo" class="tab">
+			<div class="card">
+				@include('core::admin.info.phpinfo', ['info' => $model->getPhpInfo()])
 			</div>
 		</div>
 	</div>
 	@csrf
+
 </form>
 @stop
