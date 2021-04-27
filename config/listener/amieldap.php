88acacf0ab39c6b66de5ae4763318f8db7287d08
Shawn Rice <rices@purdue.edu>
2021-04-27 11:07:38 -0400

    fix: Sync changes from default config

--- config/listener/amieldap.php
+++ config/listener/amieldap.php
@@ -5,9 +5,9 @@ return [
     | Amie LDAP credentials
     |--------------------------------------------------------------------------
     */
-    'hosts'            => ['amie.anvil.rcac.purdue.edu'],
-    'use_ssl'          => false,
-    'base_dn'          => env('LDAP_AMIE_BASEDN', conf('ldap_amie', 'basedn', 'dc=rcac,dc=purdue,dc=edu')),
-    'username'         => env('LDAP_AMIE_USERNAME', conf('ldap_amie', 'rdn', 'cn=halcyon,dc=rcac,dc=purdue,dc=edu')),
-    'password'         => env('LDAP_AMIE_PASSWORD', conf('ldap_amie', 'pass', '')),
+    'hosts'    => ['amie.anvil.rcac.purdue.edu'],
+    'use_ssl'  => false,
+    'base_dn'  => env('LDAP_AMIE_BASEDN', conf('ldap_amie', 'basedn', 'dc=rcac,dc=purdue,dc=edu')),
+    'username' => env('LDAP_AMIE_USERNAME', conf('ldap_amie', 'rdn', 'cn=halcyon,dc=rcac,dc=purdue,dc=edu')),
+    'password' => env('LDAP_AMIE_PASSWORD', conf('ldap_amie', 'pass', '')),
 ];
