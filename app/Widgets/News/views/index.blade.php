34e02e920397af8d29531bec2b84e465ac6936fd
Shawn Rice <rices@purdue.edu>
2021-05-07 11:15:57 -0400

    fix(news): Fix wrong param name

--- app/Widgets/News/views/index.blade.php
+++ app/Widgets/News/views/index.blade.php
@@ -6,8 +6,8 @@
 <div class="news">
 	<?php if ($params->get('show_title')): ?>
 		<{{ $params->get('item_heading', 'h3') }}>
-			<?php if ($params->get('newstypeid')): ?>
-				{{ $type->name }}
+			<?php if ($params->get('catid')): ?>
+				{{ $params->get('title') ? $params->get('title') : $type->name }}
 			<?php else: ?>
 				{{ $params->get('title') ? $params->get('title') : trans('widget.news::news.news') }}
 			<?php endif; ?>
@@ -30,7 +30,7 @@
 		</ul>
 	<?php else: ?>
 		<ul class="newslist">
-			@if ($params->get('newstypeid'))
+			@if ($params->get('catid'))
 				<li class="first">{{ trans('widget.news::news.no type found', ['type' => $type->name]) }}</li>
 			@else
 				<li class="first">{{ trans('widget.news::news.no articles found') }}</li>
@@ -39,7 +39,7 @@
 	<?php endif; ?>
 	<?php if ($params->get('more')): ?>
 		<div class="more">
-			<a href="<?php echo ($params->get('newstypeid') ? route('site.news.type', ['name' => $type->name]) : route('site.news.index')); ?>">
+			<a href="<?php echo ($params->get('catid') ? route('site.news.type', ['name' => $type->name]) : route('site.news.index')); ?>">
 				{{ trans('widget.news::news.more') }}
 			</a>
 		</div>
