034482a1ea866f7ec6af08de25769d7741a1f661
Shawn Rice <rices@purdue.edu>
2021-05-26 09:09:59 -0400

    fix(queues): Remove hard-coded hostname

--- app/Modules/Queues/Resources/views/mail/queueremoved/manager.blade.php
+++ app/Modules/Queues/Resources/views/mail/queueremoved/manager.blade.php
@@ -15,7 +15,7 @@ Access to your {{ config('app.name') }} resources (resources, queues, and Unix g
 
 ---
 
-Check the [Group History](https://www.rcac.purdue.edu/account/history/) to see who made this change.
+Check the [Group History]({{ route('site.users.account.section', ['section' => 'history']) }}) to see who made this change.
 
 If any of these are incorrect you can make changes at any time on the [Queue Management website]({{ route('site.users.account') }}). Changes to your queue's authorized user list are effective within a few minutes, however, account removals (if appropriate) are completed during overnight processing. If a person's access to all {{ config('app.name') }} resources is removed they will be able to access their home directory files and Fortress files for as long as they have a current Purdue Career Account. Information regarding the access of these files will be sent to these persons once accounts are removed.
 
