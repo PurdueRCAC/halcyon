96ade18a4608512db3f0ead70aa86abbf7797b12
Shawn Rice <rices@purdue.edu>
2021-06-07 08:34:09 -0400

    fix(news): Remove unused lines

--- app/Modules/News/Http/siteRoutes.php
+++ app/Modules/News/Http/siteRoutes.php
@@ -16,7 +16,7 @@ $router->group(['prefix' => 'news'], function (Router $router)
 	$router->get('rss/{name}', [
 		'as' => 'site.news.feed',
 		'uses' => 'ArticlesController@feed',
-	]);//->where('name', '[a-zA-Z0-9\-_,]+');
+	]);//->where('name', '[a-zA-Z0-9\-_,\+\% ]+');
 	$router->get('search', [
 		'as' => 'site.news.search',
 		'uses' => 'ArticlesController@search',
@@ -40,32 +40,22 @@ $router->group(['prefix' => 'news'], function (Router $router)
 	$router->get('{id}', [
 		'as' => 'site.news.show',
 		'uses' => 'ArticlesController@show',
-		//'middleware' => 'can:tag.tags.edit',
 	])->where('id', '[0-9]+');
 	$router->get('{id}/edit', [
 		'as' => 'site.news.edit',
 		'uses' => 'ArticlesController@edit',
-		//'middleware' => 'can:tag.tags.edit',
 	])->where('id', '[0-9]+');
 	$router->put('{id}', [
 		'as' => 'site.news.update',
 		'uses' => 'ArticlesController@update',
-		//'middleware' => 'can:tag.tags.edit',
 	])->where('id', '[0-9]+');
 	$router->delete('{id}', [
 		'as' => 'site.news.destroy',
 		'uses' => 'ArticlesController@destroy',
-		//'middleware' => 'can:tag.tags.destroy',
 	])->where('id', '[0-9]+');
 
 	$router->get('{name}', [
 		'as' => 'site.news.type',
 		'uses' => 'ArticlesController@type',
-		//'middleware' => 'can:tag.tags.index',
-	])->where('name', '[a-zA-Z\-_ ]+');
+	])->where('name', '[a-zA-Z\-_\+\% ]+');
 });
-
-/*$router->get('coffee', [
-	'as' => 'site.news.coffee',
-	'uses' => 'ArticlesController@coffee',
-]);*/
