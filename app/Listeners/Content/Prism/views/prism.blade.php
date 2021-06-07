ada828462792a04182d7d6f6a38fbfd7984c2337
Shawn Rice <rices@purdue.edu>
2021-06-07 13:38:48 -0400

    fix: Temp disable cache busting for listener

--- app/Listeners/Content/Prism/views/prism.blade.php
+++ app/Listeners/Content/Prism/views/prism.blade.php
@@ -1,7 +1,7 @@
 @push('scripts')
-<script src="{{ asset('listeners/content/prism/vendor/prism/prism.js?v=' . filemtime(public_path() . '/listeners/content/prism/vendor/prism/prism.js')) }}"></script>
+<script src="{{ asset('listeners/content/prism/vendor/prism/prism.js') }}"></script>
 @endpush
 
 @push('styles')
-<link rel="stylesheet" type="text/css" href="{{ asset('listeners/content/prism/vendor/prism/prism.css?v=' . filemtime(public_path() . '/listeners/content/prism/vendor/prism/prism.css')) }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('listeners/content/prism/vendor/prism/prism.css') }}" />
 @endpush
