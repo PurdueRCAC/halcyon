c1b25a42da83349df67a3826c3d882b5a5085f98
Shawn Rice <rices@purdue.edu>
2021-06-08 15:19:01 -0400

    fix: Ensure object is found

--- app/Modules/Storage/Resources/views/site/directories/group.blade.php
+++ app/Modules/Storage/Resources/views/site/directories/group.blade.php
@@ -460,7 +460,7 @@
 										{
 											$p = $parent->toArray();
 											$p['permissions'] = json_decode(json_encode($parent->unixPermissions), true);
-											$p['unixgroup'] = $parent->unixgroup->toArray();
+											$p['unixgroup'] = $parent->unixgroup ? $parent->unixgroup->toArray() : array('longname' => '');
 											$childs[] = $p;
 										}
 
