8ca238499d1a33455012be42773d8cffc0c092f0
Shawn Rice <rices@purdue.edu>
2021-06-09 14:08:19 -0400

    fix(storage): Add more info to legacy output

--- app/Modules/Storage/Http/Resources/DirectoryResource.php
+++ app/Modules/Storage/Http/Resources/DirectoryResource.php
@@ -18,7 +18,7 @@ class DirectoryResource extends JsonResource
 
 		$data['api'] = route('api.storage.directories.read', ['id' => $this->id]);
 		$data['group'] = $this->group;
-		$data['unixgroup'] = $this->unixgroup;
+		$data['unixgroup'] = $this->unixgroup->toArray();
 		$data['autounixgroup'] = $this->autounixgroup;
 
 		// Set directory type
@@ -36,6 +36,50 @@ class DirectoryResource extends JsonResource
 		$data['acl'] = $this->acl;
 		$data['children'] = $this->children;
 		$data['latestusage'] = $this->usage()->orderBy('id', 'desc')->first();
+
+		$data['user'] = array(
+			'id' => 0,
+			'name' => 'root'
+		);
+
+		if ($this->owner)
+		{
+			$data['user'] = array(
+				'id' => $this->owner->id,
+				'name' => $this->owner->username
+			);
+		}
+
+		// [!] Legacy compatibility
+		if (request()->segment(1) == 'ws')
+		{
+			$data['path'] = $this->fullPath;
+			$data['id'] = '/ws/storagedir/' . $this->id;
+
+			$data['group']['id'] = '/ws/group/' . $this->id;
+
+			$data['resource'] = array(
+				'id' => '/ws/resource/' . $this->resourceid,
+				'name' => $this->resource->name
+			);
+
+			$data['user']['uid'] = $data['user']['id'];
+			unset($data['user']['id']);
+
+			if (!empty($data['unixgroup']))
+			{
+				$data['unixgroup']['id'] = '/ws/unixgroup/' . $data['unixgroup']['id'];
+				$data['unixgroup']['gid'] = $data['unixgroup']['groupid'];
+				unset($data['unixgroup']['groupid']);
+			}
+
+			$data['childdirs'] = array();
+			foreach ($data['children'] as $child)
+			{
+				$data['childdirs'][] = '/ws/storagedir/' . $child->id;
+			}
+			unset($data['children']);
+		}
 
 		return $data;
 	}
