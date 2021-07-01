d4d3b855f14714150f30da0f3f4f9534282e0899
Shawn Rice <rices@purdue.edu>
2021-07-01 14:33:45 -0400

    feat: Add user lookup event for RCAC LDAP

--- app/Listeners/Users/RcacLdap/RcacLdap.php
+++ app/Listeners/Users/RcacLdap/RcacLdap.php
@@ -3,7 +3,8 @@ namespace App\Listeners\Users\RcacLdap;
 
 use App\Modules\Users\Events\UserSearching;
 use App\Modules\Users\Events\UserBeforeDisplay;
-use App\Modules\Users\Events\UserDisplay;
+//use App\Modules\Users\Events\UserDisplay;
+use App\Modules\Users\Events\UserLookup;
 use App\Modules\Users\Models\User;
 use App\Modules\Courses\Events\CourseEnrollment;
 use App\Modules\Resources\Events\ResourceMemberStatus;
@@ -28,6 +29,7 @@ class RcacLdap
 	{
 		$events->listen(UserSearching::class, self::class . '@handleUserSearching');
 		$events->listen(UserBeforeDisplay::class, self::class . '@handleUserBeforeDisplay');
+		$events->listen(UserLookup::class, self::class . '@handleUserLookup');
 		//$events->listen(UserDisplay::class, self::class . '@handleUserDisplay');
 		$events->listen(ResourceMemberStatus::class, self::class . '@handleResourceMemberStatus');
 		$events->listen(UnixGroupFetch::class, self::class . '@handleUnixGroupFetch');
@@ -649,5 +651,103 @@ class RcacLdap
 		}
 
 		$this->log('ldap', __METHOD__, 'GET', $status, $results, 'host=scholar.rcac.purdue.edu');
+	}
+
+	/**
+	 * Handle a user lookup event
+	 * 
+	 * Look for a user in the Purdue LDAP based on the specified
+	 * criteria and return a User object based on the first match.
+	 *
+	 * @param   UserLookup  $event
+	 * @return  void
+	 */
+	public function handleUserLookup(UserLookup $event)
+	{
+		$config = $this->config();
+
+		if (empty($config))
+		{
+			return;
+		}
+
+		$criteria = $event->criteria;
+		$query = [];
+		$results = array();
+
+		foreach ($criteria as $key => $val)
+		{
+			switch ($key)
+			{
+				case 'puid':
+				case 'organization_id':
+					// `employeeNumber` needs to be 10 digits in length for the query to work
+					//    ex: 12345678 -> 0012345678
+					$val = str_pad($val, 10, '0', STR_PAD_LEFT);
+					$query[] = ['employeeNumber', '=', $val];
+				break;
+
+				case 'username':
+					$query[] = ['uid', '=', $val];
+				break;
+
+				case 'host':
+					$query[] = [$key, '=', $val];
+				break;
+
+				case 'name':
+				default:
+					$query[] = ['cn', '=', $val];
+				break;
+			}
+		}
+
+		if (empty($query))
+		{
+			return;
+		}
+
+		try
+		{
+			$ldap = $this->connect($config);
+
+			$status = 404;
+
+			// Performing a query.
+			$data = $ldap->search()
+				->where($query)
+				->select(['cn', 'uid', 'employeeNumber'])
+				->get();
+
+			if (!empty($data))
+			{
+				$status = 200;
+
+				foreach ($data as $key => $result)
+				{
+					$user = User::findByUsername($result['uid'][0]);
+					if (!$user || !$user->id)
+					{
+						$user = new User;
+					}
+					$user->name = $result['cn'][0];
+					$user->getUserUsername()->username = $result['uid'][0];
+					$user->puid = $result['employeeNumber'][0];
+
+					//$event->user = $user;
+					//break;
+					$results[$key] = $user;
+				}
+			}
+
+			$event->results = $results;
+		}
+		catch (\Exception $e)
+		{
+			$status = 500;
+			$results = ['error' => $e->getMessage()];
+		}
+
+		$this->log('ldap', __METHOD__, 'GET', $status, $results, json_encode($query));
 	}
 }
