39abb5b7147942b37e3c275a97acfb753a9be122
Shawn Rice <rices@purdue.edu>
2021-07-09 15:10:11 -0400

    fix(news): Ensure landmarks have unique labels

--- app/Modules/News/Resources/views/site/index.blade.php
+++ app/Modules/News/Resources/views/site/index.blade.php
@@ -28,7 +28,7 @@ app('pathway')->append(
 						</h2>
 					</div>
 					<div class="col-md-4 text-right">
-						<div class="btn-group" role="navigation" aria-label="Calendar options">
+						<div class="btn-group" role="navigation" aria-label="Calendar options for {{ $type->name }}">
 							<a class="btn btn-default tip" href="{{ route('site.news.feed', ['name' => $type->name]) }}" title="{{ trans('news::news.rss feed') }}">
 								<i class="fa fa-rss-square" aria-hidden="true"></i><span class="sr-only">{{ trans('news::news.rss feed') }}</span>
 							</a>
