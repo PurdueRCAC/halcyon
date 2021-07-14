109c73c28c5900b75f0b58c662939e410e4587da
Shawn Rice <rices@purdue.edu>
2021-07-14 06:56:05 -0400

    refactor(rcac): Update assets for new homepage

--- app/Themes/Rcac/assets/js/page/homepage.js
+++ app/Themes/Rcac/assets/js/page/homepage.js
@@ -1,8 +1,10 @@
 /* global $ */  // jquery.js
 
 $(document).ready(function() {
-	$('.bxslider').bxSlider({
-		auto: true,
-		autoControls: true
+	$(window).on('scroll', function(e){
+		// what the y position of the scroll is
+		var y = $(window).scrollTop();
+		//$('.hero').css('background-position', 'right ' + ((y / $(window).height()) * 100) + 'px top 0');
+		$('.hero').css('background-position', 'right -' + (y / 3) + 'px')
 	});
 });
