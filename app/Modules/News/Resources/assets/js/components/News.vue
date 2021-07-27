e8055cb52484ec98b9395d808cc78c29787bedf3
Jean Benedict Nugroho <jnugroho@purdue.edu>
2021-07-26 19:29:23 -0500

    Clean up unused pagination comment block

--- app/Modules/News/Resources/assets/js/components/News.vue
+++ app/Modules/News/Resources/assets/js/components/News.vue
@@ -172,25 +172,7 @@
             @update="update"
             @delete="del"
         ></news-article>
-        <!-- <nav aria-label="Page navigation example">
-            <ul class="pagination">
-                <li class="page-item">
-                    <a class="page-link" href="#">Previous</a>
-                </li>
-                <li class="page-item">
-                    <span class="page-link" @click="read">1</span>
-                </li>
-                <li class="page-item">
-                    <span class="page-link" @click="read">2</span>
-                </li>
-                <li class="page-item">
-                    <span class="page-link" @click="read">3</span>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
-                </li>
-            </ul>
-        </nav> -->
+
         <nav aria-label="navigation">
             <ul class="pagination">
                 <li
