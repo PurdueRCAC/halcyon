972cdb40d6a5146b95bde2b4208dcdb9a87f19f2
Jean Benedict Nugroho <jnugroho@purdue.edu>
2021-08-16 17:40:17 -0500

    Attempt to use Vue Router

---
+++ app/Modules/News/Resources/assets/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h3>TESTING</h3>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App'
+    }
+</script>
