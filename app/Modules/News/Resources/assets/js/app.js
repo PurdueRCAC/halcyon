a9fd9f47eb053901873da1d63f7feabca3e88719
Jean Benedict Nugroho <jnugroho@purdue.edu>
2021-08-17 15:08:51 -0400

    Revert "Attempt to use Vue Router"
    This reverts commit 972cdb40d6a5146b95bde2b4208dcdb9a87f19f2.

--- app/Modules/News/Resources/assets/js/app.js
+++ app/Modules/News/Resources/assets/js/app.js
@@ -3,71 +3,52 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
- require("./bootstrap");
- // Add a request interceptor
- window.axios.interceptors.request.use(
-     function(config) {
-         // assume your access token is stored in local storage
-         // (it should really be somewhere more secure but I digress for simplicity)
-         let token = document.head
-             .querySelector('meta[name="api-token"]')
-             .getAttribute("content"); //localStorage.getItem('access_token')
-         if (token) {
-             config.headers["Authorization"] = `Bearer ${token}`;
-         }
-         return config;
-     },
-     function(error) {
-         // Do something with request error
-         return Promise.reject(error);
-     }
- );
- window.Vue = require("vue");
- let baseurl = document.head.querySelector('meta[name="base-url"]');
- if (baseurl) {
-     Vue.prototype.ROOT_URL = baseurl.content;
- } else {
-     Vue.prototype.ROOT_URL = "";
- }
- import Vue from 'vue';
- /**
-  * The following block of code may be used to automatically register your
-  * Vue components. It will recursively scan this directory for the Vue
-  * components and automatically register them with their "basename".
-  *
-  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
-  */
- 
-//  import VueRouter from 'vue-router';
- Vue.component("VueRouter", require("vue-router"));
-//  import News from './components/site/News.vue';
- Vue.component("News", require("./components/site/News.vue"));
- 
- Vue.use(VueRouter);
- 
- const router = new VueRouter({
-     mode: 'history',
-     routes: [
-         { path: '/news/search', component: News },
-         { path: '*', redirect: '/news/search' }
-     ]
- });
- 
- // const files = require.context('./', true, /\.vue$/i);
- // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
- // Vue.component("news-search", require("./components/site/News.vue").default);
- Vue.component("App", require("./App.vue").default);
- //import News from './components/News.vue';
- /**
-  * Next, we will create a fresh Vue application instance and attach it to
-  * the page. Then, you may begin adding components to this application
-  * or customize the JavaScript scaffolding to fit your unique needs.
-  */
- const app = new Vue({
-     el: "#app",
-     router
-     /*components: {
-           News
-       },
-       render: h => h(News)*/
- });
+require("./bootstrap");
+// Add a request interceptor
+window.axios.interceptors.request.use(
+    function(config) {
+        // assume your access token is stored in local storage
+        // (it should really be somewhere more secure but I digress for simplicity)
+        let token = document.head
+            .querySelector('meta[name="api-token"]')
+            .getAttribute("content"); //localStorage.getItem('access_token')
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    function(error) {
+        // Do something with request error
+        return Promise.reject(error);
+    }
+);
+window.Vue = require("vue");
+let baseurl = document.head.querySelector('meta[name="base-url"]');
+if (baseurl) {
+    Vue.prototype.ROOT_URL = baseurl.content;
+} else {
+    Vue.prototype.ROOT_URL = "";
+}
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+// const files = require.context('./', true, /\.vue$/i);
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+Vue.component("news-search", require("./components/site/News.vue").default);
+//import News from './components/News.vue';
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+const app = new Vue({
+    el: "#app"
+    /*components: {
+          News
+      },
+      render: h => h(News)*/
+});
