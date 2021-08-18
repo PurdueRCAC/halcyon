aa2215619e861e92b5682232ef66ac545ec2e28c
Shawn Rice <rices@purdue.edu>
2021-08-18 15:01:39 -0400

    fix: Adjust banner styles

--- app/Widgets/Notice/views/index.blade.php
+++ app/Widgets/Notice/views/index.blade.php
@@ -3,14 +3,14 @@
 @endpush
 
 @if ($publish)
+<div class="notice-banner" role="banner">
 	<div id="{{ $id }}" class="notice alert alert-{{ $alertlevel . ($params->get('allowClose', 1) ? ' alert-dismissible' : '') }} mb-0">
-		<p class="mb-0">
-			{!! $message !!}
-			@if ($params->get('allowClose', 1))
-				<button type="button" class="close" data-dismiss="alert" aria-label="{{ trans('widget.notice::notice.close') }}">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			@endif
-		</p>
+		{!! $message !!}
+		@if ($params->get('allowClose', 1))
+			<button type="button" class="close" data-dismiss="alert" aria-label="{{ trans('widget.notice::notice.close') }}">
+				<span aria-hidden="true">&times;</span>
+			</button>
+		@endif
 	</div>
+</div>
 @endif
