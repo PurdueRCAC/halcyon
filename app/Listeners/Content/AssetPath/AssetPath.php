adeaa59a5e5e469840a90519ac1efbc45a94fc33
Shawn Rice <rices@purdue.edu>
2021-08-18 07:38:15 -0400

    fix: Don't match attributes like `data-src`

--- app/Listeners/Content/AssetPath/AssetPath.php
+++ app/Listeners/Content/AssetPath/AssetPath.php
@@ -27,21 +27,26 @@ class AssetPath
 	 */
 	public function handlePageContentIsRendering(PageContentIsRendering $event)
 	{
-		$content = preg_replace_callback('/src="(.*?)"/i', function($matches)
+		$content = preg_replace_callback('/\ssrc="(.*?)"/i', function($matches)
 		{
 			if (substr($matches[1], 0, 4) == 'http')
 			{
-				return 'src="' . $matches[1] . '"';
+				return ' src="' . $matches[1] . '"';
 			}
 
-			if (substr($matches[1], 0, 6) == '/files')
+			if (substr($matches[1], 0, 7) == '/files/')
+			{
+				$matches[1] = substr($matches[1], 7);
+			}
+
+			if (substr($matches[1], 0, 6) == 'files/')
 			{
 				$matches[1] = substr($matches[1], 6);
 			}
 
-			return 'src="' . asset("files/" . ltrim($matches[1], '/')) . '"';
+			return ' src="' . asset("files/" . ltrim($matches[1], '/')) . '"';
 		}, $event->getBody());
-		$content = preg_replace('/src="\/include\/images\/(.*?)"/i', 'src="' . asset("files/$1") . '"', $content);
+		$content = preg_replace('/ src="\/include\/images\/(.*?)"/i', ' src="' . asset("files/$1") . '"', $content);
 
 		$content = preg_replace('/href="\/(.*?)"/i', 'href="' . url("$1") . '"', $content);
 
