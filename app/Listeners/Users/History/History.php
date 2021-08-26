d29db7de4b57feeffc353b1cc78ea4c3cd2eebf7
Shawn Rice <rices@purdue.edu>
2021-08-26 16:37:21 -0400

    refactor(history): Single, searchable timeline

--- app/Listeners/Users/History/History.php
+++ app/Listeners/Users/History/History.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Listeners\Users\History;
 
+use Illuminate\Support\Fluent;
 use App\Modules\Users\Events\UserDisplay;
 use App\Modules\History\Models\Log;
 use App\Modules\Listeners\Models\Listener;
@@ -65,11 +66,27 @@ class History
 				->where('userid', '=', $user->id)
 				->where('transportmethod', '!=', 'GET')
 				->paginate(config('list_limit', 20));*/
+			$items = collect([]);
 
 			$groups = $user->groups()
 				->withTrashed()
 				->orderBy('datecreated', 'desc')
 				->get();
+
+			foreach ($groups as $g)
+			{
+				$item = new Fluent;
+				$item->id = $g->id;
+				$item->route = route('site.users.account.section.show.subsection', ['section' => 'groups', 'id' => $g->groupid, 'subsection' => 'members', 'u' => $user->id != auth()->user()->id ? $user->id : null]);
+				$item->type = 'group';
+				$item->subtype = ($g->type ? $g->type->name : trans('global.unknown'));
+				$item->description = $g->group->name;
+				$item->created = $g->datecreated;
+				$item->removed = $g->dateremoved;
+				$item->isTrashed = $g->isTrashed();
+
+				$items->push($item);
+			}
 
 			$unixgroups = \App\Modules\Groups\Models\UnixGroupMember::query()
 				->withTrashed()
@@ -77,17 +94,97 @@ class History
 				->orderBy('datetimecreated', 'desc')
 				->get();
 
+			foreach ($unixgroups as $g)
+			{
+				$ug = $g->unixgroup()->withTrashed()->first();
+
+				$item = new Fluent;
+				$item->id = $g->id;
+				$item->route = route('site.users.account.section.show', ['section' => 'groups', 'id' => $ug->groupid, 'u' => $user->id != auth()->user()->id ? $user->id : null]);
+				$item->type = 'unix group';
+				$item->description = $ug->longname . ' ' . ($ug && $ug->group ? '(' . $ug->group->name . ')' : trans('global.unknown'));
+				$item->created = $g->datetimecreated;
+				$item->removed = $g->datetimeremoved;
+				$item->isTrashed = $g->isTrashed();
+
+				$items->push($item);
+			}
+
 			$queues = \App\Modules\Queues\Models\User::query()
 				->withTrashed()
 				->where('userid', '=', $user->id)
 				->orderBy('datetimecreated', 'desc')
 				->get();
 
+			foreach ($queues as $g)
+			{
+				$q = $g->queue()
+					->withTrashed()
+					->first();
+
+				$item = new Fluent;
+				$item->id = $g->id;
+				$item->route = route('site.users.account.section.show.subsection', ['section' => 'groups', 'id' => $q->groupid, 'subsection' => 'queues', 'u' => $user->id != auth()->user()->id ? $user->id : null]);
+				$item->type = 'queue';
+				$item->description = $q->name . ' ' . ($q->resource ? '(' . $q->resource->name . ')' : trans('global.unknown'));
+				$item->created = $g->datetimecreated;
+				$item->removed = $g->datetimeremoved;
+				$item->isTrashed = $g->isTrashed();
+
+				if ($g->isTrashed())
+				{
+					$item->isTrashed = $g->isTrashed();
+				}
+				elseif ($q)
+				{
+					if ($q->isTrashed())
+					{
+						$item->isTrashed = $q->isTrashed();
+						$item->removed = $q->datetimeremoved;
+					}
+					else
+					{
+						if ($q->resource && $q->resource->isTrashed())
+						{
+							$item->isTrashed = $q->resource->isTrashed();
+							$item->removed = $q->resource->datetimeremoved;
+						}
+					}
+				}
+
+				$items->push($item);
+			}
+
 			$courses = \App\Modules\Courses\Models\Member::query()
 				->withTrashed()
 				->where('userid', '=', $user->id)
 				->orderBy('datetimecreated', 'desc')
 				->get();
+
+			foreach ($courses as $g)
+			{
+				$class = $g->account()
+					->withTrashed()
+					->first();
+
+				$item = new Fluent;
+				$item->id = $g->id;
+				$item->route = route('site.users.account.section', ['section' => 'class', 'u' => $user->id != auth()->user()->id ? $user->id : null]);
+				$item->type = 'class';
+				if ($class->semester == 'Workshop')
+				{
+					$item->description = $class->classname;
+				}
+				else
+				{
+					$item->description = $class->department . ' ' . $class->coursenumber . ' (' . $class->crn . ')';
+				}
+				$item->created = $g->datetimecreated;
+				$item->removed = $g->datetimeremoved;
+				$item->isTrashed = $g->isTrashed();
+
+				$items->push($item);
+			}
 
 			$classes = \App\Modules\Courses\Models\Account::query()
 				->withTrashed()
@@ -98,27 +195,30 @@ class History
 
 			foreach ($classes as $class)
 			{
-				$member = new \App\Modules\Courses\Models\Member;
-				$member->classaccountid = $class->id;
-				$member->userid = $class->userid;
-				$member->datetimecreated = $class->datetimecreated;
-				$member->datetimeremoved = $class->datetimeremoved;
-				$member->datetimestart = $class->datetimestart;
-				$member->datetimestop = $class->datetimestop;
-				$member->membertype = 2;
+				$item = new Fluent;
+				$item->id = $class->id;
+				$item->route = route('site.users.account.section', ['section' => 'class', 'u' => $user->id != auth()->user()->id ? $user->id : null]);
+				$item->type = 'class';
+				if ($class->account->semester == 'Workshop')
+				{
+					$item->description = $class->account->classname;
+				}
+				else
+				{
+					$item->description = $class->account->department . ' ' . $class->account->coursenumber . ' (' . $class->account->crn . ')';
+				}
+				$item->created = $class->datetimestart;
+				$item->removed = $class->datetimestop;
+				$item->isTrashed = $class->isTrashed();
 
-				$courses->push($member);
+				$items->push($item);
 			}
 
-			$courses = $courses->sortByDesc('datetimecreated');
+			$history = $items->sortByDesc('created');
 
 			$content = view('history::site.profile', [
 				'user'    => $user,
-				'groups'  => $groups,
-				'unixgroups'  => $unixgroups,
-				'queues'  => $queues,
-				'courses' => $courses,
-				//'history' => $history,
+				'history' => $history,
 			]);
 		}
 
--- app/Modules/History/Resources/views/site/profile.blade.php
+++ app/Modules/History/Resources/views/site/profile.blade.php
@@ -1,254 +1,121 @@
+@push('styles')
+<link rel="stylesheet" type="text/css" media="all" href="{{ asset('modules/core/vendor/datatables/dataTables.bootstrap4.min.css?v=' . filemtime(public_path() . '/modules/core/vendor/datatables/dataTables.bootstrap4.min.css')) }}" />
+@endpush
+
+@push('scripts')
+<script src="{{ asset('modules/core/vendor/datatables/datatables.min.js?v=' . filemtime(public_path() . '/modules/core/vendor/datatables/datatables.min.js')) }}"></script>
+<script src="{{ asset('modules/core/vendor/datatables/dataTables.bootstrap4.min.js?v=' . filemtime(public_path() . '/modules/core/vendor/datatables/dataTables.bootstrap4.min.js')) }}"></script>
+<script>
+$(document).ready(function() {
+	if ($('.datatable').length) {
+		$('.datatable').DataTable({
+			pageLength: 20,
+			pagingType: 'numbers',
+			info: false,
+			ordering: false,
+			lengthChange: false,
+			scrollX: true,
+			autoWidth: false,
+			language: {
+				searchPlaceholder: "Filter history...",
+				search: "_INPUT_",
+			},
+			initComplete: function () {
+				this.api().columns().every(function (i) {
+					if (i == 0) {
+						var column = this;
+						var select = $('<select class="form-control form-control-sm" data-index="' + i + '"><option value="">' + $(column.header()).html() + '</option></select>')
+							.appendTo($(column.header()).empty());
+		
+						column.data().unique().sort().each(function (d, j) {
+							select.append('<option value="'+d+'">'+d+'</option>');
+						});
+					}
+				});
+
+				var table = this;
+
+				$(table.api().table().container()).on('change', 'thead select', function () {
+					var val = $.fn.dataTable.util.escapeRegex(
+						$(this).val()
+					);
+
+					table.api()
+						.column($(this).data('index'))
+						.search(val ? '^'+val+'$' : '', true, false)
+						.draw();
+				});
+			}
+		});
+	}
+
+	//$(".tabs").tabs();
+});
+</script>
+@endpush
 
 <div class="contentInner">
 	<h2>{{ trans('history::history.history') }}</h2>
 
-	@if (count($groups) || count($unixgroups) || count($queues) || count($courses))
-		@if (count($groups) > 0)
-			<table class="table table-hover">
-				<caption>Group History</caption>
-				<thead>
+	<?php /*<div class="tabs">
+		<ul>
+			<li>
+				<a href="#memberships">Memberships</a>
+			</li>
+			<li>
+				<a href="#activity">Activity</a>
+			</li>
+		</ul>
+		<div id="memberships"> */ ?>
+	@if (count($history) > 0)
+		<table class="table table-hover datatable">
+			<caption>Memberships</caption>
+			<thead>
+				<tr>
+					<th scope="col">Type</th>
+					<th scope="col">Name</th>
+					<th scope="col">Added</th>
+					<th scope="col">Removed</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ($history as $item)
 					<tr>
-						<th scope="col">Role</th>
-						<th scope="col">Group</th>
-						<th scope="col">Added</th>
-						<th scope="col">Removed</th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach ($groups as $group)
-						<tr>
-							<td>
-								{{ $group->type ? $group->type->name : trans('global.unknown') }}
-							</td>
-							<td>
-								<a href="{{ route('site.users.account.section.show.subsection', ['section' => 'groups', 'id' => $group->groupid, 'subsection' => 'members', 'u' => $user->id != auth()->user()->id ? $user->id : null]) }}">
-									{{ $group->group ? $group->group->name : trans('global.unknown') }}
+						<td>
+							{{ $item->type }}
+						</td>
+						<td>
+							@if ($item->route)
+								<a href="{{ $item->route }}">
+							@endif
+							{{ $item->description }}
+							@if ($item->route)
 								</a>
-							</td>
-							<td>
-								<time datetime="{{ $group->datecreated->toDateTimeString() }}">
-									{{ $group->datecreated->format('M d, Y') }}
-								</time>
-							</td>
-							<td>
-								@if ($group->isTrashed())
-									<time datetime="{{ $group->dateremoved->toDateTimeString() }}">
-										@if ($group->dateremoved->getTimestamp() > Carbon\Carbon::now()->getTimestamp())
-											{{ $group->dateremoved->diffForHumans() }}
-										@else
-											{{ $group->dateremoved->format('M d, Y') }}
-										@endif
-									</time>
-								@else
-									-
-								@endif
-							</td>
-						</tr>
-					@endforeach
-				</tbody>
-			</table>
-		@endif
-
-		@if (count($unixgroups) > 0)
-			<table class="table table-hover">
-				<caption>Unix Group History</caption>
-				<thead>
-					<tr>
-						<th scope="col">Group</th>
-						<th scope="col">Unix Group</th>
-						<th scope="col">Added</th>
-						<th scope="col">Removed</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					foreach ($unixgroups as $group):
-						$ug = $group->unixgroup()->withTrashed()->first();
-						?>
-						<tr id="unixgroup{{ $group->id }}">
-							<td>
-								<a href="{{ route('site.users.account.section.show', ['section' => 'groups', 'id' => $ug->groupid, 'u' => $user->id != auth()->user()->id ? $user->id : null]) }}">
-									{{ $ug->group ? $ug->group->name : trans('global.unknown') }}
-								</a>
-							</td>
-							<td>
-								<a href="{{ route('site.users.account.section.show', ['section' => 'groups', 'id' => $ug->groupid, 'u' => $user->id != auth()->user()->id ? $user->id : null]) }}">
-									{{ $ug ? $ug->longname : trans('global.unknown') }}
-								</a>
-							</td>
-							<td>
-								<time datetime="{{ $group->datetimecreated->toDateTimeString() }}">
-									{{ $group->datetimecreated->format('M d, Y') }}
-								</time>
-							</td>
-							<td>
-								@if ($group->isTrashed())
-									<time datetime="{{ $group->datetimeremoved->toDateTimeString() }}">
-										@if ($group->datetimeremoved->getTimestamp() > Carbon\Carbon::now()->getTimestamp())
-											{{ $group->datetimeremoved->diffForHumans() }}
-										@else
-											{{ $group->datetimeremoved->format('M d, Y') }}
-										@endif
-									</time>
-								@else
-									-
-								@endif
-							</td>
-						</tr>
-						<?php
-					endforeach;
-					?>
-				</tbody>
-			</table>
-		@endif
-
-		@if (count($queues) > 0)
-			<table class="table table-hover">
-				<caption>Queue History</caption>
-				<thead>
-					<tr>
-						<th scope="col">Resource</th>
-						<th scope="col">Queue</th>
-						<th scope="col">Added</th>
-						<th scope="col">Removed</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					foreach ($queues as $queue):
-						$q = $queue->queue()
-							->withTrashed()
-							->first();
-						?>
-						<tr id="queueuser{{ $queue->id }}">
-							<td>
-								@if ($q)
-									{{ $q->resource ? $q->resource->name : trans('global.unknown') }}
-								@else
-									{{ trans('global.unknown') }}
-								@endif
-							</td>
-							<td>
-								<a href="{{ route('site.users.account.section.show.subsection', ['section' => 'groups', 'id' => $q->groupid, 'subsection' => 'queues', 'u' => $user->id != auth()->user()->id ? $user->id : null]) }}">
-								{{ $q ? $q->name : trans('global.unknown') }}
-								</a>
-							</td>
-							<td>
-								<time datetime="{{ $queue->datetimecreated->toDateTimeString() }}">
-									@if ($queue->datetimecreated->getTimestamp() > Carbon\Carbon::now()->getTimestamp())
-										{{ $queue->datetimecreated->diffForHumans() }}
+							@endif
+							@if ($item->subtype)
+								&nbsp; <span class="badge badge-info">{{ $item->subtype }}</span>
+							@endif
+						</td>
+						<td>
+							<time datetime="{{ $item->created->toDateTimeString() }}">
+								{{ $item->created->format('M d, Y') }}
+							</time>
+						</td>
+						<td>
+							@if ($item->isTrashed)
+								<time datetime="{{ $item->removed->toDateTimeString() }}">
+									@if ($item->removed->getTimestamp() > Carbon\Carbon::now()->getTimestamp())
+										{{ $item->removed->diffForHumans() }}
 									@else
-										{{ $queue->datetimecreated->format('M d, Y') }}
+										{{ $item->removed->format('M d, Y') }}
 									@endif
 								</time>
-							</td>
-							<td>
-								@if ($queue->isTrashed())
-									<time datetime="{{ $queue->datetimeremoved->toDateTimeString() }}">
-										@if ($queue->datetimeremoved->getTimestamp() > Carbon\Carbon::now()->getTimestamp())
-											{{ $queue->datetimeremoved->diffForHumans() }}
-										@else
-											{{ $queue->datetimeremoved->format('M d, Y') }}
-										@endif
-									</time>
-								@elseif ($q)
-									@if ($q->isTrashed())
-										<time datetime="{{ $q->datetimeremoved->toDateTimeString() }}">
-											@if ($q->datetimeremoved->getTimestamp() > Carbon\Carbon::now()->getTimestamp())
-												{{ $q->datetimeremoved->diffForHumans() }}
-											@else
-												{{ $q->datetimeremoved->format('M d, Y') }}
-											@endif
-										</time>
-									@else
-										@if ($q->resource && $q->resource->isTrashed())
-											<time datetime="{{ $q->resource->datetimeremoved->toDateTimeString() }}">
-												@if ($q->resource->datetimeremoved->getTimestamp() > Carbon\Carbon::now()->getTimestamp())
-													{{ $q->resource->datetimeremoved->diffForHumans() }}
-												@else
-													{{ $q->resource->datetimeremoved->format('M d, Y') }}
-												@endif
-											</time>
-										@else
-											-
-										@endif
-									@endif
-								@else
-									-
-								@endif
-							</td>
-						</tr>
-						<?php
-					endforeach;
-					?>
-				</tbody>
-			</table>
-		@endif
-
-		@if (count($courses) > 0)
-			<table class="table table-hover">
-				<caption>Class Account History</caption>
-				<thead>
-					<tr>
-						<th scope="col">Resource</th>
-						<th scope="col">Class</th>
-						<th scope="col">Semester</th>
-						<th scope="col">Added</th>
-						<th scope="col">Removed</th>
+							@endif
+						</td>
 					</tr>
-				</thead>
-				<tbody>
-					<?php
-					foreach ($courses as $member):
-						$class = $member->account()
-							->withTrashed()
-							->first();
-						?>
-						<tr id="classuser{{ $member->id }}">
-							<td>
-								{{ $class->resource ? $class->resource->name : trans('global.unknown') }}
-							</td>
-							<td>
-								<a href="{{ route('site.users.account.section', ['section' => 'class', 'u' => $user->id != auth()->user()->id ? $user->id : null]) }}">
-								@if ($class->semester == 'Workshop')
-									{{ $class->classname }}
-								@else
-									{{ $class->department . ' ' . $class->coursenumber . ' (' . $class->crn . ')' }}
-								@endif
-								</a>
-							</td>
-							<td>
-								{{ $class->semester }}
-							</td>
-							<td>
-								<time datetime="{{ $member->datetimecreated->toDateTimeString() }}">
-									@if ($member->datetimecreated->getTimestamp() > Carbon\Carbon::now()->getTimestamp())
-										{{ $member->datetimecreated->diffForHumans() }}
-									@else
-										{{ $member->datetimecreated->format('M d, Y') }}
-									@endif
-								</time>
-							</td>
-							<td>
-								@if ($member->isTrashed())
-									<time datetime="{{ $member->datetimeremoved->toDateTimeString() }}">
-										@if ($member->datetimeremoved->getTimestamp() > Carbon\Carbon::now()->getTimestamp())
-											{{ $member->datetimeremoved->diffForHumans() }}
-										@else
-											{{ $member->datetimeremoved->format('M d, Y') }}
-										@endif
-									</time>
-								@else
-									-
-								@endif
-							</td>
-						</tr>
-						<?php
-					endforeach;
-					?>
-				</tbody>
-			</table>
-		@endif
+				@endforeach
+			</tbody>
+		</table>
 	@else
 		<div class="card card-help">
 			<div class="card-body">
@@ -257,4 +124,9 @@
 			</div>
 		</div>
 	@endif
+		<?php /* </div>
+		<div id="activity">
+			More here.
+		</div>
+	</div> */ ?>
 </div>
