a4fc66ae97c0831aa3d0ab978f6fa9206f93a112
Shawn Rice <rices@purdue.edu>
2021-09-28 09:29:50 -0400

    fix(groups): Add legacy route, fix typo

--- app/Modules/Groups/Http/wsRoutes.php
+++ app/Modules/Groups/Http/wsRoutes.php
@@ -18,11 +18,35 @@ $router->group(['prefix' => 'unixgroup', 'middleware' => 'auth.ip'], function (R
 		'uses' => 'UnixGroupsController@read',
 	])->where('id', '[0-9]+');
 	$router->match(['put', 'post'], '{id}', [
-		'as' => 'ws.unixgroupe.update',
+		'as' => 'ws.unixgroup.update',
 		'uses' => 'UnixGroupsController@update',
 	])->where('id', '[0-9]+');
 	$router->delete('{id}', [
 		'as' => 'ws.unixgroup.delete',
 		'uses' => 'UnixGroupsController@delete',
+	])->where('id', '[0-9]+');
+});
+
+$router->group(['prefix' => 'groupmotd'], function (Router $router)
+{
+	$router->get('/', [
+		'as' => 'ws.groupmotd.usage',
+		'uses' => 'MotdController@index',
+	]);
+	$router->post('/', [
+		'as' => 'ws.groupmotd.create',
+		'uses' => 'MotdController@create',
+	]);
+	$router->get('{id}', [
+		'as' => 'ws.groupmotd.read',
+		'uses' => 'MotdController@read',
+	])->where('id', '[0-9]+');
+	$router->match(['put', 'post'], '{id}', [
+		'as' => 'ws.groupmotd.update',
+		'uses' => 'MotdController@update',
+	])->where('id', '[0-9]+');
+	$router->delete('{id}', [
+		'as' => 'ws.groupmotd.delete',
+		'uses' => 'MotdController@delete',
 	])->where('id', '[0-9]+');
 });
