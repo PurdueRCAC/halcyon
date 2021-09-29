5184a2ef78fcfad8a14ac76bfb43f16ec846d21b
Shawn Rice <rices@purdue.edu>
2021-09-29 14:08:00 -0400

    feat(history): Add Logs endpoints

---
+++ app/Modules/History/Http/Controllers/Api/LogsController.php
@@ -0,0 +1,429 @@
+<?php
+
+namespace App\Modules\History\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use App\Modules\History\Models\Log;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+/**
+ * Logs
+ *
+ * @apiUri    /api/logs
+ */
+class LogsController extends Controller
+{
+	/**
+	 * Display a listing of entries
+	 *
+	 * @apiMethod GET
+	 * @apiUri    /api/logs
+	 * @apiParameter {
+	 * 		"in":            "query",
+	 * 		"name":          "action",
+	 * 		"description":   "Action taken.",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "string",
+	 * 			"enum": [
+	 * 				"create",
+	 * 				"update",
+	 * 				"delete"
+	 * 			]
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "query",
+	 * 		"name":          "type",
+	 * 		"description":   "The type of item (model name) that the action was taken on.",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "search"
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "query",
+	 * 		"name":          "search",
+	 * 		"description":   "A word or phrase to search for.",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "search"
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "query",
+	 * 		"name":          "limit",
+	 * 		"description":   "Number of result per page.",
+	 * 		"type":          "integer",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "integer",
+	 * 			"default":   25
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "query",
+	 * 		"name":          "page",
+	 * 		"description":   "Number of where to start returning results.",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "integer",
+	 * 			"default":   1
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "query",
+	 * 		"name":          "order",
+	 * 		"description":   "Field to sort results by.",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "string",
+	 * 			"default":   "created_at",
+	 * 			"enum": [
+	 * 				"id",
+	 * 				"created_at",
+	 * 				"action"
+	 * 			]
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "query",
+	 * 		"name":          "order_dir",
+	 * 		"description":   "Direction to sort results by.",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "string",
+	 * 			"default":   "asc",
+	 * 			"enum": [
+	 * 				"asc",
+	 * 				"desc"
+	 * 			]
+	 * 		}
+	 * }
+	 * @param  Request $request
+	 * @return Response
+	 */
+	public function index(Request $request)
+	{
+		// Get filters
+		$filters = array(
+			'status'    => $request->input('status', null),
+			'ip'        => $request->input('ip', null),
+			'userid'    => $request->input('userid', null),
+			'transportmethod' => $request->input('transportmethod', null),
+			'classname' => $request->input('classname', null),
+			'classmethod' => $request->input('classmethod', null),
+			'objectid' => $request->input('objectid', null),
+			'groupid' => $request->input('groupid', null),
+			'targetuserid' => $request->input('targetuserid', null),
+			'targetobjectid' => $request->input('targetobjectid', null),
+			'app'       => $request->input('app', null),
+			'search'    => $request->input('search', null),
+			'limit'     => $request->input('limit', config('list_limit', 20)),
+			'order'     => $request->input('order', Log::$orderBy),
+			'order_dir' => $request->input('order_dir', Log::$orderDir),
+			'action'    => $request->input('action', null),
+			'type'      => $request->input('type', null)
+		);
+
+		if (!in_array($filters['order'], ['id', 'name']))
+		{
+			$filters['order'] = Log::$orderBy;
+		}
+
+		if (!in_array($filters['order_dir'], ['asc', 'desc']))
+		{
+			$filters['order_dir'] = Log::$orderDir;
+		}
+
+		$query = Log::query();
+
+		if ($filters['search'])
+		{
+			$query->where(function($query) use ($filters)
+			{
+				$query->where('uri', 'like', '%' . $filters['search'] . '%')
+					->orWhere('payload', 'like', '%' . $filters['search'] . '%');
+			});
+		}
+
+		if ($filters['ip'])
+		{
+			$query->where('ip', '=', $filters['ip']);
+		}
+
+		if ($filters['status'])
+		{
+			$query->where('status', '=', $filters['status']);
+		}
+
+		if ($filters['transportmethod'])
+		{
+			$query->where('transportmethod', '=', $filters['transportmethod']);
+		}
+
+		if ($filters['classname'])
+		{
+			$query->where('classname', '=', $filters['classname']);
+		}
+
+		if ($filters['classmethod'])
+		{
+			$query->where('classmethod', '=', $filters['classmethod']);
+		}
+
+		if ($filters['userid'])
+		{
+			$query->where('userid', '=', $filters['userid']);
+		}
+
+		if ($filters['objectid'])
+		{
+			$query->where('objectid', '=', $filters['objectid']);
+		}
+
+		if ($filters['groupid'])
+		{
+			$query->where('groupid', '=', $filters['groupid']);
+		}
+
+		if ($filters['targetuserid'])
+		{
+			$query->where('targetuserid', '=', $filters['targetuserid']);
+		}
+
+		if ($filters['targetobjectid'])
+		{
+			$query->where('targetobjectid', '=', $filters['targetobjectid']);
+		}
+
+		$rows = $query
+			->orderBy($filters['order'], $filters['order_dir'])
+			->paginate($filters['limit']);
+
+		$rows->each(function ($row, $key)
+		{
+			$row->url = route('api.logs.read', ['id' => $row->id]);
+		});
+
+		return new ResourceCollection($rows);
+	}
+
+	/**
+	 * Create a news article type
+	 *
+	 * @apiMethod POST
+	 * @apiUri    /api/news/types
+	 * @apiAuthorization  true
+	 * @apiParameter {
+	 * 		"in":            "body",
+	 * 		"name":          "comment",
+	 * 		"description":   "The comment being made",
+	 * 		"required":      true,
+	 * 		"schema": {
+	 * 			"type":      "string"
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "body",
+	 * 		"name":          "name",
+	 * 		"description":   "The name of the type",
+	 * 		"required":      true,
+	 * 		"schema": {
+	 * 			"type":      "string",
+	 * 			"maxLength": 32
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "body",
+	 * 		"name":          "tagresources",
+	 * 		"description":   "Allow articles to tag resources",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "integer",
+	 * 			"default":   0,
+	 * 			"enum": [
+	 * 				0,
+	 * 				1
+	 * 			]
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "body",
+	 * 		"name":          "tagusers",
+	 * 		"description":   "Allow articles to tag users",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "integer",
+	 * 			"default":   0,
+	 * 			"enum": [
+	 * 				0,
+	 * 				1
+	 * 			]
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "body",
+	 * 		"name":          "location",
+	 * 		"description":   "Allow articles to set location",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "integer",
+	 * 			"default":   0,
+	 * 			"enum": [
+	 * 				0,
+	 * 				1
+	 * 			]
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "body",
+	 * 		"name":          "future",
+	 * 		"description":   "Allow articles to set future",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "integer",
+	 * 			"default":   0,
+	 * 			"enum": [
+	 * 				0,
+	 * 				1
+	 * 			]
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "body",
+	 * 		"name":          "ongoing",
+	 * 		"description":   "Allow articles to set ongoing",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "integer",
+	 * 			"default":   0,
+	 * 			"enum": [
+	 * 				0,
+	 * 				1
+	 * 			]
+	 * 		}
+	 * }
+	 * @apiParameter {
+	 * 		"in":            "body",
+	 * 		"name":          "url",
+	 * 		"description":   "A URL associated with the news article",
+	 * 		"required":      false,
+	 * 		"schema": {
+	 * 			"type":      "integer",
+	 * 			"default":   0,
+	 * 			"enum": [
+	 * 				0,
+	 * 				1
+	 * 			]
+	 * 		}
+	 * }
+	 * @apiResponse {
+	 * 		"200": {
+	 * 			"description": "Successful entry creation",
+	 * 			"content": {
+	 * 				"application/json": {
+	 * 					"example": {
+	 * 						"id":            "1",
+	 * 						"name":          "Examples",
+	 * 						"tagresources":  0,
+	 * 						"tagusers":      1,
+	 * 						"location":      1,
+	 * 						"future":        1,
+	 * 						"calendar":      1,
+	 * 						"url":           1,
+	 * 						"api":           "https://example.com/api/news/types/1"
+	 * 					}
+	 * 				}
+	 * 			}
+	 * 		},
+	 * 		"415": {
+	 * 			"description": "Invalid data"
+	 * 		}
+	 * }
+	 * @param   Request  $request
+	 * @return  Response
+	 */
+	public function create(Request $request)
+	{
+		$rules = [
+			'name' => 'required|string|max:32',
+			'tagresources' => 'nullable|boolean',
+			'location' => 'nullable|boolean',
+			'future' => 'nullable|boolean',
+			'ongoing' => 'nullable|boolean',
+			'tagusers' => 'nullable|boolean',
+			'calendar' => 'nullable|boolean',
+			'url' => 'nullable|url',
+		];
+
+		$validator = Validator::make($request->all(), $rules);
+
+		if ($validator->fails())
+		{
+			return response()->json(['message' => $validator->messages()], 415);
+		}
+
+		$row = new Log($request->all());
+
+		if (!$row->save())
+		{
+			return response()->json(['message' => trans('global.messages.creation failed')], 500);
+		}
+
+		$row->api = route('api.logs.read', ['id' => $row->id]);
+
+		return new JsonResource($row);
+	}
+
+	/**
+	 * Read a news article type
+	 *
+	 * @apiMethod GET
+	 * @apiUri    /api/news/types/{id}
+	 * @apiParameter {
+	 * 		"in":            "path",
+	 * 		"name":          "id",
+	 * 		"description":   "Entry identifier",
+	 * 		"required":      true,
+	 * 		"schema": {
+	 * 			"type":      "integer"
+	 * 		}
+	 * }
+	 * @apiResponse {
+	 * 		"200": {
+	 * 			"description": "Successful entry read",
+	 * 			"content": {
+	 * 				"application/json": {
+	 * 					"example": {
+	 * 						"id":            "1",
+	 * 						"name":          "Examples",
+	 * 						"tagresources":  0,
+	 * 						"tagusers":      1,
+	 * 						"location":      1,
+	 * 						"future":        1,
+	 * 						"calendar":      1,
+	 * 						"url":           1,
+	 * 						"api":           "https://example.com/api/news/types/1"
+	 * 					}
+	 * 				}
+	 * 			}
+	 * 		},
+	 * 		"404": {
+	 * 			"description": "Record not found"
+	 * 		}
+	 * }
+	 * @param  integer  $id
+	 * @return Response
+	 */
+	public function read($id)
+	{
+		$row = Log::findOrFail((int)$id);
+
+		$row->api = route('api.logs.read', ['id' => $row->id]);
+
+		return new JsonResource($row);
+	}
+}
--- app/Modules/History/Http/apiRoutes.php
+++ app/Modules/History/Http/apiRoutes.php
@@ -3,16 +3,30 @@
 use Illuminate\Routing\Router;
 
 /** @var Router $router */
-$router->group(['prefix' => 'history'], function (Router $router)
+$router->group(['prefix' => 'history', 'middleware' => 'auth:api'], function (Router $router)
 {
 	$router->get('/', [
 		'as'   => 'api.history.index',
 		'uses' => 'HistoryController@index',
-		'middleware' => 'auth:api',
 	]);
 	$router->get('{id}', [
 		'as'   => 'api.history.read',
 		'uses' => 'HistoryController@read',
-		'middleware' => 'auth:api',
+	])->where('id', '[0-9]+');
+});
+
+$router->group(['prefix' => 'logs', 'middleware' => 'auth:api'], function (Router $router)
+{
+	$router->get('/', [
+		'as'   => 'api.logs.index',
+		'uses' => 'LogsController@index',
+	]);
+	$router->post('/', [
+		'as' => 'api.logs.create',
+		'uses' => 'LogsController@create',
+	]);
+	$router->get('{id}', [
+		'as' => 'api.logs.read',
+		'uses' => 'LogsController@read',
 	])->where('id', '[0-9]+');
 });
