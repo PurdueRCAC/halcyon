d2fbea54b1fac327d406e2d55ce68f95a6ba0398
Shawn Rice <rices@purdue.edu>
2021-10-12 07:26:33 -0400

    fix(news): Use correct Bootstrap class names

--- app/Modules/News/Resources/views/site/menu.blade.php
+++ app/Modules/News/Resources/views/site/menu.blade.php
@@ -1,14 +1,14 @@
 <nav aria-label="News Categories">
-	<ul class="dropdown-menu">
+	<ul class="nav">
 		@if (auth()->user() && auth()->user()->can('manage news'))
-			<li<?php if (!is_numeric($active) && $active == 'manage') { echo ' class="active"'; } ?>><a href="{{ route('site.news.manage') }}">{{ trans('news::news.manage news') }}</a></li>
+			<li class="nav-item<?php if (!is_numeric($active) && $active == 'manage') { echo ' active'; } ?>"><a class="nav-link" href="{{ route('site.news.manage') }}">{{ trans('news::news.manage news') }}</a></li>
 		@endif
-		<li<?php if (!is_numeric($active) && $active == 'search') { echo ' class="active"'; } ?>><a href="{{ route('site.news.search') }}">{{ trans('news::news.search news') }}</a></li>
-		<li<?php if (!is_numeric($active) && $active == 'feeds') { echo ' class="active"'; } ?>><a href="{{ route('site.news.rss') }}">{{ trans('news::news.rss feeds') }}</a></li>
+		<li class="nav-item<?php if (!is_numeric($active) && $active == 'search') { echo ' active'; } ?>"><a class="nav-link" href="{{ route('site.news.search') }}">{{ trans('news::news.search news') }}</a></li>
+		<li class="nav-item<?php if (!is_numeric($active) && $active == 'feeds') { echo ' active'; } ?>"><a class="nav-link" href="{{ route('site.news.rss') }}">{{ trans('news::news.rss feeds') }}</a></li>
 		<li><div class="separator"></div></li>
 		@foreach ($types as $type)
-			<li<?php if ($active == $type->id) { echo ' class="active"'; } ?>>
-				<a href="{{ route('site.news.type', ['name' => $type->alias]) }}">
+			<li class="nav-item<?php if ($active == $type->id) { echo ' active'; } ?>">
+				<a class="nav-link" href="{{ route('site.news.type', ['name' => $type->alias]) }}">
 					{{ $type->name }}
 				</a>
 			</li>
