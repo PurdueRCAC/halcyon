4ab8d67e0dff457040764872527ea60ad4631470
Shawn Rice <rices@purdue.edu>
2021-10-19 14:59:34 -0400

    test: Adding ESLint config for linting JS

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        //"jquery": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        //"jquery",
+    ],
+    "rules": {
+    }
+};
