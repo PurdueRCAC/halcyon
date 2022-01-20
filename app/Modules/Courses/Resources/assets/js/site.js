cc47dae96ccd94214063c0445701f53846631f27
Shawn Rice <rices@purdue.edu>
2022-01-20 16:08:05 -0500

    fix(courses): Fix adding by email

--- app/Modules/Courses/Resources/assets/js/site.js
+++ app/Modules/Courses/Resources/assets/js/site.js
@@ -159,8 +159,8 @@ function AddingManyUsersEmail(xml, post_obj) {
 		}
 
 		for (var i = 0; i < response['data'].length; i++) {
-			if (response['data'][i]['username'] == post_obj['userid']
-				|| response['data'][i]['email'] == post_obj['userid']) {
+			if (response['data'][i]['username'] + '@purdue.edu' == post_obj['userid']
+			 || response['data'][i]['email'] == post_obj['userid']) {
 				if (typeof (response['data'][i]['id']) == 'undefined' || !response['data'][i]['id']) {
 					user = response['data'][i]['username'];
 				} else {
