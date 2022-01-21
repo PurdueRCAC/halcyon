9acd2748bafba683977a65398e26c813247155d3
Shawn Rice <rices@purdue.edu>
2022-01-21 14:28:39 -0500

    feat(config): Add config for notifications TODO: Maybe move this to settings on news types? The config file feels wrong for this.

---
+++ config/module/news.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'name' => 'News',
+
+	/*
+	|--------------------------------------------------------------------------
+	| List news type IDs that should send a notification upon create, update,
+	| delete.
+	|--------------------------------------------------------------------------
+	*/
+	'notify_create' => [1],
+
+	'notify_update' => [1],
+
+	'notify_delete' => [],
+];
