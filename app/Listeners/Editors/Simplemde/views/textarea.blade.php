00721121bcdd6da841cbd22acb862c52ee8632c1
Shawn Rice <rices@purdue.edu>
2022-02-21 16:44:58 -0500

    feat(simplemde): Add dark mode styles

--- app/Listeners/Editors/Simplemde/views/textarea.blade.php
+++ app/Listeners/Editors/Simplemde/views/textarea.blade.php
@@ -6,6 +6,6 @@
 <link rel="stylesheet" type="text/css" media="all" href="{{ asset('listeners/editors/simplemde/css/simplemde.min.css?v=' . filemtime(public_path() . '/listeners/editors/simplemde/css/simplemde.min.css')) }}" />
 @endpush
 @push('scripts')
-<script src="{{ asset('listeners/editors/simplemde/js/simplemde.min.js?v=' . filemtime(public_path() . '/listeners/editors/simplemde/js/simplemde.min.js')) }}"></script>
-<script src="{{ asset('listeners/editors/simplemde/js/simplemde.js?v=' . filemtime(public_path() . '/listeners/editors/simplemde/js/simplemde.js')) }}"></script>
+<script src="{{ asset('listeners/editors/simplemde/js/simplemde.min.js?v=' . filemtime(public_path('/listeners/editors/simplemde/js/simplemde.min.js'))) }}"></script>
+<script src="{{ asset('listeners/editors/simplemde/js/simplemde.js?v=' . filemtime(public_path('/listeners/editors/simplemde/js/simplemde.js'))) }}"></script>
 @endpush
