0fb1906009bece41f51cac02c69fb7a0a7cfa8fa
Shawn Rice <rices@purdue.edu>
2022-03-10 15:46:51 -0500

    fix(knowledge): Don't change absolute src URLs

--- app/Modules/Knowledge/Models/Page.php
+++ app/Modules/Knowledge/Models/Page.php
@@ -219,7 +219,26 @@ class Page extends Model
 		$text = preg_replace('/href="\/(.*?)"/i', 'href="' . url("$1") . '"', $text);
 
 		// Fix file paths
-		$text = preg_replace('/src="(.*?)"/i', 'src="' . asset("files/$1") . '"', $text);
+		$text = preg_replace_callback('/\ssrc="(.*?)"/i', function($matches)
+		{
+			if (substr($matches[1], 0, 4) == 'http')
+			{
+				return ' src="' . $matches[1] . '"';
+			}
+
+			if (substr($matches[1], 0, 7) == '/files/')
+			{
+				$matches[1] = substr($matches[1], 7);
+			}
+
+			if (substr($matches[1], 0, 6) == 'files/')
+			{
+				$matches[1] = substr($matches[1], 6);
+			}
+
+			return ' src="' . asset("files/" . ltrim($matches[1], '/')) . '"';
+		}, $text);
+		//$text = preg_replace('/src="(.*?)"/i', 'src="' . asset("files/$1") . '"', $text);
 		$text = preg_replace('/src="\/include\/images\/(.*?)"/i', 'src="' . asset("files/$1") . '"', $text);
 		$text = preg_replace('/href="\/(.*?)"/i', 'href="' . url("$1") . '"', $text);
 
