ec30d3d1fd0cf4d82177e5de039cc4e76151a3b5
Shawn Rice <rices@purdue.edu>
2022-04-01 08:49:36 -0400

    refactor(issues): Remove usage of jQuery

--- app/Modules/Issues/Resources/assets/js/admin.js
+++ app/Modules/Issues/Resources/assets/js/admin.js
@@ -4,72 +4,135 @@
 /**
  * Initiate event hooks
  */
-document.addEventListener('DOMContentLoaded', function() {
-	$('.issue-todo').on('change', function () {
-		var that = $(this);
+document.addEventListener('DOMContentLoaded', function () {
+	document.querySelectorAll('.issue-todo').forEach(function (el) {
+		el.addEventListener('change', function () {
+			var that = this;
 
-		if (that.is(':checked')) {
-			$.ajax({
-				url: that.data('api'),
-				type: 'post',
-				data: {
-					'report': that.data('name'),
-					'issuetodoid': that.data('id')
-				},
-				dataType: 'json',
-				async: false,
-				success: function (data) {
-					$(that.closest('li')).fadeOut(400, function () {
-						$(this).addClass('hide').removeAttr('style');
-						$(that.closest('li')).addClass('complete');
-						$(that.closest('li')).removeClass('incomplete');
-						that.data('issue', data.id);
-						$('#checklist_status').trigger('change');
+			if (that.checked) {
+				fetch(that.getAttribute('data-api'), {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+							'Authorization': 'Bearer ' + document.querySelector('meta[name="api-token"]').getAttribute('content')
+						},
+						body: JSON.stringify({
+							'report': that.getAttribute('data-name'),
+							'issuetodoid': that.getAttribute('data-id')
+						})
+					})
+					.then(function (response) {
+						if (response.ok) {
+							return response.json();
+						}
+						return response.json().then(function (data) {
+							var msg = data.message;
+							if (typeof msg === 'object') {
+								msg = Object.values(msg).join('<br />');
+							}
+							throw msg;
+						});
+					})
+					.then(function (data) {
+						var li = that.closest('li');
+
+						var fadeEffect = setInterval(function () {
+							if (!li.style.opacity) {
+								li.style.opacity = 1;
+							}
+							if (li.style.opacity > 0) {
+								li.style.opacity -= 0.1;
+							} else {
+								clearInterval(fadeEffect);
+
+								li.classList.add('hide');
+								li.style = '';
+								li.classList.add('complete');
+								li.classList.remove('incomplete');
+								that.setAttribute('data-issue', data.id);
+
+								document.getElementById('checklist_status').dispatchEvent(new Event('change', { bubbles: true }));
+							}
+						}, 200);
+					})
+					.catch(function (error) {
+						var img = that.closest('li').querySelector('.fa');
+						img.className = "fa fa-exclamation-triangle";
+						img.parentNode.title = "Unable to save changes, reload the page and try again.";
+
+						Halcyon.message('danger', error);
 					});
-				},
-				error: function (xhr) { //xhr, ajaxOptions, thrownError
-					var img = $(that.closest('li')).find('.fa')[0];
-					img.className = "fa fa-exclamation-triangle";
-					img.parentNode.title = "Unable to save changes, reload the page and try again.";
+			} else if (that.getAttribute('data-issue')) {
+				fetch(that.getAttribute('data-api') + '/' + that.getAttribute('data-issue'), {
+						method: 'DELETE',
+						headers: {
+							'Content-Type': 'application/json',
+							'Authorization': 'Bearer ' + document.querySelector('meta[name="api-token"]').getAttribute('content')
+						}
+					})
+					.then(function (response) {
+						if (response.ok) {
+							return response.json();
+						}
+						return response.json().then(function (data) {
+							var msg = data.message;
+							if (typeof msg === 'object') {
+								msg = Object.values(msg).join('<br />');
+							}
+							throw msg;
+						});
+					})
+					.then(function () {
+						var li = that.closest('li');
 
-					Halcyon.message('danger', xhr.responseJSON.message);
-				}
-			});
-		} else if (that.data('issue')) {
-			$.ajax({
-				url: that.data('api') + '/' + that.data('issue'),
-				type: 'delete',
-				dataType: 'json',
-				async: false,
-				success: function () {
-					$(that.closest('li')).fadeOut(400, function () {
-						$(this).addClass('hide').removeAttr('style');
-						$(that.closest('li')).removeClass('complete');
-						$(that.closest('li')).addClass('incomplete');
-						that.data('issue', '');
-						$('#checklist_status').trigger('change');
+						var fadeEffect = setInterval(function () {
+							if (!li.style.opacity) {
+								li.style.opacity = 1;
+							}
+							if (li.style.opacity > 0) {
+								li.style.opacity -= 0.1;
+							} else {
+								clearInterval(fadeEffect);
+
+								li.classList.add('hide');
+								li.style = '';
+								li.classList.add('incomplete');
+								li.classList.remove('complete');
+								that.setAttribute('data-issue', '');
+
+								document.getElementById('checklist_status').dispatchEvent(new Event('change', { bubbles: true }));
+							}
+						}, 200);
+					})
+					.catch(function (error) {
+						var img = that.closest('li').querySelector('.fa');
+						img.className = "fa fa-exclamation-triangle";
+						img.parentNode.title = "Unable to save changes, reload the page and try again.";
+
+						Halcyon.message('danger', error);
 					});
-				},
-				error: function (xhr) {
-					var img = $(that.closest('li')).find('.fa')[0];
-					img.className = "fa fa-exclamation-triangle";
-					img.parentNode.title = "Unable to save changes, reload the page and try again.";
-
-					Halcyon.message('danger', xhr.responseJSON.message);
-				}
-			});
-		}
+			}
+		});
 	});
 
-	$('#checklist_status').on('change', function(){
-		var val = $(this).val();
-		if (val == 'all') {
-			$('.checklist>li').removeClass('hide');
-		} else {
-			$('.checklist>li').addClass('hide');
-			$('.' + val).removeClass('hide');
-		}
-	});
+	var status = document.getElementById('checklist_status');
+	if (status) {
+		status.addEventListener('change', function () {
+			var val = this.value;
+			if (val == 'all') {
+				document.querySelectorAll('.checklist>li').forEach(function (el) {
+					el.classList.remove('hide');
+				});
+			} else {
+				document.querySelectorAll('.checklist>li').forEach(function (el) {
+					el.classList.add('hide');
+				});
+				document.querySelectorAll('.' + val).forEach(function (el) {
+					el.classList.remove('hide');
+				});
+			}
+		});
+	}
 
 	$('.basic-multiple').select2({
 		placeholder: $(this).data('placeholder')
@@ -77,167 +140,200 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	$('.searchable-select').select2();
 
-	$('.comments-show').on('click', function (e) {
-		e.preventDefault();
-		$($(this).attr('href')).toggleClass('hide');
-	});
-
-	$('.comment-add').on('click', function (e) {
-		e.preventDefault();
-
-		var btn = $(this);
-		var container = $(btn.data('parent'));
-		var comment = $('#' + container.attr('id') + '_comment');
-		var resolution = $('#' + container.attr('id') + '_resolution');
-		var post = {
-			'issueid': $('#id').val(),
-			'comment': comment.val(),
-			'resolution': resolution.is(':checked') ? 1 : 0
-		};
-		// create new relationship
-		$.ajax({
-			url: container.data('api'),
-			type: 'post',
-			data: post,
-			dataType: 'json',
-			async: false,
-			success: function (response) {
-				Halcyon.message('success', 'Item added');
-
-				var c = container.parent();
-				var li = c.find('li.d-none');
-
-				if (typeof (li) !== 'undefined') {
-					var template = $(li)
-						.clone()
-						.removeClass('d-none');
-
-					template
-						.attr('id', template.attr('id').replace(/\{id\}/g, response.id))
-						.data('api', response.api);
-
-					template.find('a').each(function (i, el) {
-						$(el).attr('href', $(el).attr('href').replace(/\{id\}/g, response.id));
-					});
-					template.find('textarea').each(function (i, el) {
-						$(el).attr('id', $(el).attr('id').replace(/\{id\}/g, response.id));
-						//$(el).val(response.comment);
-					});
-					template.find('label').each(function (i, el) {
-						$(el).attr('for', $(el).attr('for').replace(/\{id\}/g, response.id));
-					});
-					template.find('button').each(function (i, el) {
-						$(el).attr('data-parent', $(el).attr('data-parent').replace(/\{id\}/g, response.id));
-					});
-					if (response.resolution) {
-						template.find('.badge-success').each(function (i, el) {
-							$(el).removeClass('hide');
-						});
-					}
-
-					template.find('p').each(function (i, el) {
-						$(el).html(
-							$(el).html()
-								.replace(/\{who\}/g, response.username)
-								.replace(/\{when\}/g, response.datetimecreated)
-						);
-					});
-
-					template.find('div').each(function (i, el) {
-						if ($(el).attr('id')) {
-							$(el).attr('id', $(el).attr('id').replace(/\{id\}/g, response.id));
-							if ($(el).attr('id').match(/_text$/)) {
-								$(el).html(response.formattedcomment);
-							}
-						}
-					});
-
-					var content = template
-						.html()
-						.replace(/\{id\}/g, response.id);
-
-					template.html(content).insertBefore(li);
-
-					$('#comment_' + response.id + '_comment').val(response.comment);
-				}
-
-				comment.val('');
-			},
-			error: function (xhr) {
-				//console.log(xhr);
-				Halcyon.message('danger', xhr.responseJSON.message);
-			}
+	document.querySelectorAll('.comments-show').forEach(function (el) {
+		el.addEventListener('click', function (e) {
+			e.preventDefault();
+			document.getElementById(this.getAttribute('href')).classList.toggle('hide');
 		});
 	});
 
-	$('#main')
-		.on('click', '.comment-edit,.comment-cancel', function (e) {
+	document.querySelectorAll('.comment-add').forEach(function (el) {
+		el.addEventListener('click', function (e) {
 			e.preventDefault();
 
-			$(this).closest('li').toggleClass('is-editing');
-			//$('#' + container.attr('id') + '_text').toggleClass('d-none');
-			//$('#' + container.attr('id') + '_edit').toggleClass('d-none');
-			//$(this).toggleClass('d-none');
-		})
-		.on('click', '.comment-delete', function (e) {
-			e.preventDefault();
+			var btn = this;
+			var container = document.querySelector(btn.getAttribute('data-parent'));
+			var comment = document.getElementById(container.getAttribute('id') + '_comment');
+			var resolution = document.getElementById(container.getAttribute('id') + '_resolution');
+			var post = {
+				'issueid': document.getElementById('field-id').value,
+				'comment': comment.value,
+				'resolution': resolution.checked ? 1 : 0
+			};
 
-			var result = confirm($(this).data('confirm'));
-
-			if (result) {
-				var field = $($(this).attr('href'));
-
-				$.ajax({
-					url: field.data('api'),
-					type: 'delete',
-					dataType: 'json',
-					async: false,
-					success: function () {
-						Halcyon.message('success', 'Item removed');
-						field.remove();
+			fetch(container.getAttribute('data-api'), {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': 'Bearer ' + document.querySelector('meta[name="api-token"]').getAttribute('content')
 					},
-					error: function (xhr) {
-						Halcyon.message('danger', xhr.responseJSON.message);
+					body: JSON.stringify(post)
+				})
+				.then(function (response) {
+					if (response.ok) {
+						return response.json();
 					}
+					return response.json().then(function (data) {
+						var msg = data.message;
+						if (typeof msg === 'object') {
+							msg = Object.values(msg).join('<br />');
+						}
+						throw msg;
+					});
+				})
+				.then(function (data) {
+					Halcyon.message('success', 'Item added');
+
+					var c = container.parentNode;
+					var li = c.querySelector('li.d-none');
+
+					if (typeof (li) !== 'undefined') {
+						var template = li.cloneNode(true);
+						template.classList.remove('d-none');
+						template.setAttribute('id', template.getAttribute('id').replace(/\{id\}/g, data.id))
+						template.setAttribute('data-api', data.api);
+
+						template.querySelectorAll('a').forEach(function (el) {
+							el.setAttribute('href', el.getAttribute('href').replace(/\{id\}/g, data.id));
+						});
+						template.querySelectorAll('textarea').forEach(function (el) {
+							el.setAttribute('id', el.getAttribute('id').replace(/\{id\}/g, data.id));
+						});
+						template.querySelectorAll('label').forEach(function (el) {
+							el.setAttribute('for', el.getAttribute('for').replace(/\{id\}/g, data.id));
+						});
+						template.querySelectorAll('button').forEach(function (el) {
+							el.setAttribute('data-parent', el.getAttribute('data-parent').replace(/\{id\}/g, data.id));
+						});
+
+						template.querySelectorAll('.text-muted').forEach(function (el) {
+							el.innerHTML = el.innerHTML
+								.replace(/\{who\}/g, data.username)
+								.replace(/\{when\}/g, data.datetimecreated);
+						});
+
+						template.querySelectorAll('div').forEach(function (el) {
+							if (el.getAttribute('id')) {
+								el.setAttribute('id', el.getAttribute('id').replace(/\{id\}/g, data.id));
+								if (el.getAttribute('id').match(/_text$/)) {
+									el.innerHTML = data.formattedcomment;
+								}
+							}
+						});
+
+						template.setAttribute('id', template.getAttribute('id').replace(/\{id\}/g, data.id))
+						template.setAttribute('data-api', data.api);
+
+						var content = template
+							.innerHTML
+							.replace(/\{id\}/g, data.id);
+
+						template.innerHTML = content;
+						li.parentNode.insertBefore(template, li);
+
+						var ta = document.getElementById('comment_' + data.id + '_comment');
+						ta.value = data.comment;
+						ta.dispatchEvent(new Event('initEditor', { bubbles: true }));
+					}
+
+					comment.value = '';
+					comment.dispatchEvent(new Event('refreshEditor', { bubbles: true }));
+				})
+				.catch(function (error) {
+					console.log(error);
+					Halcyon.message('danger', error);
 				});
+		});
+	});
+
+	document.getElementById('main').addEventListener('click', function (e) {
+		if (e.target.parentNode.matches('.comment-edit')
+			|| e.target.matches('.comment-cancel')) {
+			e.preventDefault();
+			e.target.closest('li').classList.toggle('is-editing');
+		}
+
+		if (e.target.parentNode.matches('.comment-delete')) {
+			if (confirm(e.target.parentNode.getAttribute('data-confirm'))) {
+				var field = document.querySelector(e.target.parentNode.getAttribute('href'));
+
+				fetch(field.getAttribute('data-api'), {
+						method: 'DELETE',
+						headers: {
+							'Content-Type': 'application/json',
+							'Authorization': 'Bearer ' + document.querySelector('meta[name="api-token"]').getAttribute('content')
+						}
+					})
+					.then(function (response) {
+						if (response.ok) {
+							Halcyon.message('success', 'Comment removed');
+							field.remove();
+							return;
+						}
+
+						return response.json().then(function (data) {
+							var msg = data.message;
+							if (typeof msg === 'object') {
+								msg = Object.values(msg).join('<br />');
+							}
+							throw msg;
+						});
+					})
+					.catch(function (error) {
+						Halcyon.message('danger', error);
+					});
 			}
-		})
-		.on('click', '.comment-save', function (e) {
+		}
+
+		if (e.target.matches('.comment-save')) {
 			e.preventDefault();
 
-			//var btn = $(this);
-			var container = $(this).closest('li');
-			var comment = $('#' + container.attr('id') + '_comment');
-			var resolution = $('#' + container.attr('id') + '_resolution');
+			var container = e.target.closest('li');
+			var comment = document.getElementById(container.getAttribute('id') + '_comment');
+			var resolution = document.getElementById(container.getAttribute('id') + '_resolution');
 
-			$.ajax({
-				url: container.data('api'),
-				type: 'put',
-				data: {
-					'comment': comment.val(),
-					'resolution': resolution.is(':checked') ? 1 : 0
-				},
-				dataType: 'json',
-				async: false,
-				success: function (response) {
-					Halcyon.message('success', 'Item saved');
-					container.toggleClass('is-editing');
-					console.log(response.resolution);
-					if (response.resolution == 1) {
-						$('#comment_' + response.id).find('.badge-success').each(function (i, el) {
-							$(el).removeClass('hide');
+			fetch(container.getAttribute('data-api'), {
+					method: 'PUT',
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': 'Bearer ' + document.querySelector('meta[name="api-token"]').getAttribute('content')
+					},
+					body: JSON.stringify({
+						'comment': comment.value,
+						'resolution': resolution.checked ? 1 : 0
+					})
+				})
+				.then(function (response) {
+					if (response.ok) {
+						return response.json();
+					}
+
+					return response.json().then(function (data) {
+						var msg = data.message;
+						if (typeof msg === 'object') {
+							msg = Object.values(msg).join('<br />');
+						}
+						throw msg;
+					});
+				})
+				.then(function (data) {
+					Halcyon.message('success', 'Comment saved');
+
+					container.classList.toggle('is-editing');
+					if (data.resolution == 1) {
+						document.getElementById('comment_' + data.id).querySelectorAll('.badge-success').forEach(function (el) {
+							el.classList.remove('hide');
 						});
 					} else {
-						$('#comment_' + response.id).find('.badge-success').each(function (i, el) {
-							$(el).addClass('hide');
+						document.getElementById('comment_' + data.id).querySelectorAll('.badge-success').forEach(function (el) {
+							el.classList.remove('hide');
 						});
 					}
-					$('#comment_' + response.id + '_text').html(response.formattedcomment);
-				},
-				error: function (xhr) {
-					//console.log(xhr);
-					Halcyon.message('danger', xhr.responseJSON.message);
-				}
-			});
-		});
+					document.getElementById('comment_' + data.id + '_text').innerHTML = data.formattedcomment;
+				})
+				.catch(function (error) {
+					Halcyon.message('danger', error);
+				});
+		}
+	});
 });
