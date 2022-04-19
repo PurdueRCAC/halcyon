ecd3982fcf136899ecd7d441cf450a14c6dfc90d
Shawn Rice <rices@purdue.edu>
2022-04-19 08:57:45 -0400

    fix: Add system lang files

---
+++ app/Modules/Finder/Resources/lang/en/system.php
@@ -0,0 +1,4 @@
+<?php
+return [
+	'finder' => 'Service Finder',
+];
--- app/Modules/Users/Resources/views/site/admin.blade.php
+++ app/Modules/Users/Resources/views/site/admin.blade.php
@@ -2,12 +2,14 @@
 
 @push('styles')
 <link rel="stylesheet" type="text/css" media="all" href="{{ asset('modules/core/vendor/select2/css/select2.css?v=' . filemtime(public_path() . '/modules/core/vendor/select2/css/select2.css')) }}" />
+<link rel="stylesheet" type="text/css" media="all" href="{{ asset('modules/core/vendor/tom-select/css/tom-select.bootstrap4.min.css') }}" />
 @endpush
 
 @push('scripts')
 <script src="{{ asset('modules/core/vendor/select2/js/select2.min.js?v=' . filemtime(public_path() . '/modules/core/vendor/select2/js/select2.min.js')) }}"></script>
+<script src="{{ asset('modules/core/vendor/tom-select/js/tom-select.complete.min.js') }}"></script>
 <script>
-$(document).ready(function() {
+/*$(document).ready(function() {
 	var searchusers = $('.searchuser');
 	if (searchusers.length) {
 		searchusers.each(function(i, el){
@@ -57,6 +59,74 @@ $(document).ready(function() {
 			window.location = $(this).data('url');
 		});
 	}
+});*/
+document.addEventListener('DOMContentLoaded', function() {
+	document.querySelectorAll('.searchuser').forEach(function(el) {
+		var sel = new TomSelect(el, {
+			//maxItems: 1,
+			valueField: 'id',
+			labelField: 'name',
+			searchField: ['name', 'username'],
+			plugins: {
+				clear_button:{
+					title: 'Remove selected',
+				}
+			},
+			persist: false,
+			// Fetch remote data
+			load: function(query, callback) {
+				var url = el.getAttribute('data-api') + '?order=name&order_dir=asc&search=' + encodeURIComponent(query);
+
+				fetch(url)
+					.then(response => response.json())
+					.then(json => {
+						for (var i = 0; i < json.data.length; i++) {
+							if (!json.data[i].id) {
+								json.data[i].id = json.data[i].username;
+							}
+						}
+						callback(json.data);
+					}).catch(() => {
+						callback();
+					});
+			},
+			// Custom rendering functions for options and items
+			render: {
+				// Option list when searching
+				option: function(item, escape) {
+					if (item.name.match(/\([a-z0-9]+\)$/)) {
+						item.username = item.name.replace(/([^\(]+\()/, '').replace(/\)$/, '');
+						item.name = item.name.replace(/\s(\([a-z0-9]+\))$/, '');
+					}
+					if (isNaN(item.id) && typeof item.name != 'undefined') {
+						item.disabled = true;
+						return `<div data-id="${ escape(item.id) }">${ escape(item.name) } <span class="text-muted">(${ escape(item.username) })</span>
+							<span class="text-warning ml-1"><span class="fa fa-exclamation-triangle" aria-hidden="true"></span> ${ escape(el.getAttribute('data-noaccount')) }</span></span>
+						</div>`;
+					}
+					return `<div data-id="${ escape(item.id) }">${ escape(item.name) } <span class="text-muted">(${ escape(item.username) })</span></div>`;
+				},
+				// Selected items
+				item: function(item, escape) {
+					if (item.name.match(/\([a-z0-9]+\)$/)) {
+						if (isNaN(item.id)) {
+							item.id = item.username;
+						}
+						item.username = item.name.replace(/([^\(]+\()/, '').replace(/\)$/, '');
+						item.name = item.name.replace(/\s(\([a-z0-9]+\))$/, '');
+					}
+					return `<div data-id="${ escape(item.id) }">${ escape(item.name) }&nbsp;<span class="text-muted">(${ escape(item.username) })</span></div>`;
+					//return `<option value="${ escape(item.id) }">${ escape(item.text) }</option>`;
+				}
+			}
+		});
+		sel.on('item_add', function(item) {
+			window.location = el.getAttribute('data-url') + '?u=' + item;
+		});
+		sel.on('item_remove', function(e) {
+			window.location = el.getAttribute('data-url');
+		});
+	});
 });
 </script>
 @endpush
@@ -73,11 +143,14 @@ $(document).ready(function() {
 			@endif
 					<label for="newuser" class="sr-only">Show data for user:</label>
 					<div class="input-group">
-						<select name="u" id="newuser" class="form-control searchuser" multiple="multiple" data-placeholder="Select user..." data-api="{{ route('api.users.index') }}" data-url="{{ request()->url() }}">
+						<select name="u" id="newuser" class="form-control searchuser" multiple="multiple" data-placeholder="Select user..." data-noaccount="No local account" data-api="{{ route('api.users.index') }}" data-url="{{ request()->url() }}">
 							@if ($user->id != auth()->user()->id)
-								<option value="{{ $user->id }}" selected="selected">{{ $user->name }}</option>
+								<option value="{{ $user->id }}" selected="selected">{{ $user->name }} ({{ $user->username }})</option>
 							@endif
 						</select>
+						<!-- 
+						<input type="text" name="u" id="newuser" class="form-control searchuser" placeholder="Search for user..." data-noaccount="No local account" data-api="{{ route('api.users.index') }}" data-url="{{ request()->url() }}"<?php if ($user->id != auth()->user()->id) { echo ' value="' . $user->name . ' (' . $user->username . ')"'; } ?> />
+						 -->
 						<span class="input-group-append">
 							<button type="submit" class="btn input-group-text">
 								<span class="fa fa-search" id="add_button_a" aria-hidden="true"></span>
