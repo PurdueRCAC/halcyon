65c9bb447ea477f07e4cbf88cc6fefd1654b7429
Shawn Rice <rices@purdue.edu>
2022-04-22 15:45:33 -0400

    docs(themes): Add missing docblock

--- app/Modules/Themes/Helpers/helpers.php
+++ app/Modules/Themes/Helpers/helpers.php
@@ -1,7 +1,12 @@
 <?php
 if (!function_exists('theme_path'))
 {
-
+	/**
+	 * Get the path to a theme
+	 *
+	 * @param string $filename
+	 * @return string
+	 */
 	function theme_path($filename = null)
 	{
 		return app('themes')->themePath($filename);
