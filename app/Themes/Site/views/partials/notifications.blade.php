8ed2f8b096892d0801b0ee26965116cda7aaeb42
Shawn Rice <rices@purdue.edu>
2022-05-03 09:15:11 -0400

    fix: Handle array of messages

--- app/Themes/Site/views/partials/notifications.blade.php
+++ app/Themes/Site/views/partials/notifications.blade.php
@@ -1,20 +1,53 @@
 @if (Session::has('success'))
     <div class="alert alert-success fade in alert-dismissable">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        {{ Session::get('success') }}
+        <?php
+        $err = Session::get('success');
+
+        if (is_array($err)):
+            foreach ($err as $i => $er):
+                $err[$i] = e($er);
+                echo implode('<br />', $err);
+            endforeach;
+        else:
+            echo e($err);
+        endif;
+        ?>
     </div>
 @endif
 
 @if (Session::has('error'))
     <div class="alert alert-danger fade in alert-dismissable">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        {{ Session::get('error') }}
+        <?php
+        $err = Session::get('error');
+
+        if (is_array($err)):
+            foreach ($err as $i => $er):
+                $err[$i] = e($er);
+                echo implode('<br />', $err);
+            endforeach;
+        else:
+            echo e($err);
+        endif;
+        ?>
     </div>
 @endif
 
 @if (Session::has('warning'))
     <div class="alert alert-warning fade in alert-dismissable">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        {{ Session::get('warning') }}
+        <?php
+        $err = Session::get('warning');
+
+        if (is_array($err)):
+            foreach ($err as $i => $er):
+                $err[$i] = e($er);
+                echo implode('<br />', $err);
+            endforeach;
+        else:
+            echo e($err);
+        endif;
+        ?>
     </div>
 @endif
