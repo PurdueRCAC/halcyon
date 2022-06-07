1f22c75064b1ad152b1acb4c2a3cdb6213f71f95
Shawn Rice <zooley@purdue.edu>
2022-06-07 07:56:50 -0400

    feat: Add CAS authenticator

---
+++ app/Listeners/Auth/Cas/lang/en/cas.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	'listener name' => 'CAS',
+	'listener desc' => 'Handles user authentication against CAS',
+
+	'error_expired_ticket' => 'CAS ticket has expired',
+	'unknown_user' => 'Unknown user and new user registration is not permitted.',
+	'authentication_failed' => 'Username and password do not match or you do not have an account yet.',
+	'sign_in' => 'Sign in with Organization Account',
+];
