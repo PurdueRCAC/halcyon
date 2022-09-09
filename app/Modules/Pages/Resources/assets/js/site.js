cbdc9b8705d888fc5e7fdadccba4e3f763e2e621
Shawn Rice <zooley@purdue.edu>
2022-09-09 09:32:27 -0400

    fix(pages): Update allowed chars in aliases

--- app/Modules/Pages/Resources/assets/js/site.js
+++ app/Modules/Pages/Resources/assets/js/site.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
 	var alias = document.getElementById('field-alias');
 	if (alias) {
 		document.getElementById('field-title').addEventListener('keyup', function () {
-			alias.value = this.value.toLowerCase()
+			alias.value = this.value//.toLowerCase()
 				.replace(/\s+/g, '_')
-				.replace(/[^a-z0-9\-_]+/g, '');
+				.replace(/[^a-z0-9\-_.]+/g, '');
 		});
 	}
 
