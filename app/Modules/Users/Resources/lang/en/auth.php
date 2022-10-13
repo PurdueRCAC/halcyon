c6d4a8e3b9ca14b081845b5f4867f95e2c01ac9f
Shawn Rice <zooley@purdue.edu>
2022-10-13 10:26:55 -0400

    fix(users): Add missing lang string

--- app/Modules/Users/Resources/lang/en/auth.php
+++ app/Modules/Users/Resources/lang/en/auth.php
@@ -19,4 +19,5 @@ return [
 	'forgotten password' => 'Forgotten password',
 	'forgotten password instructions' => 'Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.',
 	'terms confirmation' => 'I have read and agree to the <a href=":url" data-toggle="modal" data-target="#termscontent">:title</a>',
+	'register' => 'Register',
 ];
