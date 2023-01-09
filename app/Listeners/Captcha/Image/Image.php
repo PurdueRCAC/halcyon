02443c77d6de7747855cf56e0b93ca24b172c73f
Shawn Rice <zooley@purdue.edu>
2023-01-09 10:40:51 -0500

    fix: Remove deprecated string offset syntax

--- app/Listeners/Captcha/Image/Image.php
+++ app/Listeners/Captcha/Image/Image.php
@@ -178,7 +178,8 @@ class Image
 				$this->keystring = '';
 				for ($i=0; $i<$length; $i++)
 				{
-					$this->keystring .= $allowed_symbols{mt_rand(0, strlen($allowed_symbols)-1)};
+					$pos = mt_rand(0, strlen($allowed_symbols)-1);
+					$this->keystring .= substr($allowed_symbols, $pos, $pos + 1);
 				}
 				if (!preg_match('/cp|cb|ck|c6|c9|rn|rm|mm|co|do|cl|db|qp|qb|dp|ww/', $this->keystring))
 				{
@@ -202,14 +203,14 @@ class Image
 
 				if (!$reading_symbol && !$transparent)
 				{
-					$font_metrics[$alphabet{$symbol}] = array('start'=>$i);
+					$font_metrics[substr($alphabet, $symbol, $symbol + 1)] = array('start'=>$i);
 					$reading_symbol = true;
 					continue;
 				}
 
 				if ($reading_symbol && $transparent)
 				{
-					$font_metrics[$alphabet{$symbol}]['end'] = $i;
+					$font_metrics[substr($alphabet, $symbol, $symbol + 1)]['end'] = $i;
 					$reading_symbol = false;
 					$symbol++;
 					continue;
@@ -227,7 +228,7 @@ class Image
 			$x = 1;
 			for ($i=0; $i<$length; $i++)
 			{
-				$m = $font_metrics[$this->keystring{$i}];
+				$m = $font_metrics[substr($this->keystring, $i, $i + 1)];
 
 				$y = mt_rand(-$fluctuation_amplitude, $fluctuation_amplitude)+($height-$fontfile_height)/2+2;
 
