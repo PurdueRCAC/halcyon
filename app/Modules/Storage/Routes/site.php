1ee378dd2224e8fb34900c9cebd3fef888300ce1
Shawn Rice <zooley@purdue.edu>
2023-01-11 07:14:30 -0500

    fix(storage): Remove conflicting route

--- app/Modules/Storage/Routes/site.php
+++ app/Modules/Storage/Routes/site.php
@@ -3,14 +3,14 @@
 use Illuminate\Routing\Router;
 
 /** @var Router $router */
-$router->group(['prefix' => 'storage'], function (Router $router)
+/*$router->group(['prefix' => 'storage'], function (Router $router)
 {
 	$router->get('/', [
 		'as' => 'site.storage.index',
 		'uses' => 'StorageController@index',
 	]);
-	/*$router->get('/{name}', [
+	$router->get('/{name}', [
 		'as' => 'site.storage.show',
 		'uses' => 'StorageController@show',
-	]);*/
-});
+	]);
+});*/
