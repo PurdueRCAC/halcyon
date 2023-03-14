df51a36d8e700899a80b65050ec01074bdedf060
Shawn Rice <zooley@purdue.edu>
2023-03-14 07:44:27 -0400

    fix(core): Set module ordering, groupings

--- app/Modules/Core/Database/Migrations/0000_00_00_000001_register_modules.php
+++ app/Modules/Core/Database/Migrations/0000_00_00_000001_register_modules.php
@@ -21,7 +21,7 @@ class RegisterModules extends Migration
 		{
 			// Register core module
 			$entry = DB::table('extensions')
-				->where('element', '=', 'core')
+				->whereIn('element', ['core', 'Core'])
 				->where('type', '=', 'module')
 				->first();
 
@@ -29,40 +29,105 @@ class RegisterModules extends Migration
 			{
 				DB::table('extensions')->insert([
 					'name'      => 'core',
-					'element'   => 'core',
+					'element'   => 'Core',
 					'type'      => 'module',
+					'folder'    => 'system',
 					'enabled'   => 1,
 					'protected' => 1,
 					'state'     => 1,
 					'access'    => 1,
+					'ordering'  => 1,
 				]);
 			}
 
 			// Auto-register other modules
 			$entries = DB::table('extensions')
 				->where('element', '!=', 'core')
+				->where('element', '!=', 'Core')
 				->where('type', '=', 'module')
 				->count();
 
 			if (!$entries)
 			{
+				$ordering = 1;
+
 				foreach (app('files')->directories(app_path('Modules')) as $dir)
 				{
-					$element = strtolower(basename($dir));
+					$element = basename($dir);
 
-					if ($element == 'core')
+					if ($element == 'Core')
 					{
 						continue;
 					}
 
+					switch ($element)
+					{
+						case 'Config':
+						case 'Cron':
+						case 'History':
+						case 'Messages':
+						case 'Dashboard':
+							$folder = 'system';
+						break;
+
+						case 'Finder':
+						case 'Issues':
+						case 'Queues':
+						case 'Resources':
+						case 'Storage':
+							$folder = 'resources';
+						break;
+
+						case 'Users':
+						case 'Groups':
+						case 'ContactReports':
+						case 'Mailer':
+							$folder = 'users';
+						break;
+
+						case 'Languages':
+						case 'Listeners':
+						case 'Widgets':
+							$folder = 'extensions';
+						break;
+
+						case 'Knowledge':
+						case 'Media':
+						case 'News':
+						case 'Pages':
+						case 'Publications':
+						case 'Tags':
+							$folder = 'content';
+						break;
+
+						default:
+							$folder = strtolower($element);
+					}
+
+					/*$ordering = 0;
+
+					$last = DB::table('extensions')
+						->where('folder', '=', $folder)
+						->where('type', '=', 'module')
+						->orderBy('ordering', 'desc')
+						->first();
+					if ($last)
+					{
+						$ordering = $last->ordering;
+					}*/
+
+					$ordering++;
+
 					DB::table('extensions')->insert([
 						'name'       => $element,
 						'element'    => $element,
+						'folder'     => $folder,
 						'type'       => 'module',
 						'enabled'    => 1,
-						'protected'  => (in_array($element, ['core', 'config', 'dashboard', 'history', 'users', 'listeners', 'widgets', 'pages', 'menus', 'media', 'tags', 'themes']) ? 1 : 0),
+						'protected'  => (in_array($element, ['Config', 'Dashboard', 'History', 'Users', 'Listeners', 'Widgets', 'Pages', 'Menus', 'Media', 'Tags', 'Themes']) ? 1 : 0),
 						'state'      => 1,
 						'access'     => 1,
+						'ordering'   => $ordering,
 					]);
 				}
 			}
