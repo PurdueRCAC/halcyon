e856c9618c06081eb0c454495b5f351a0489c5ed
Shawn Rice <zooley@purdue.edu>
2023-03-28 08:16:12 -0400

    feat: Options to output text snippet, location

--- app/Widgets/News/views/index.blade.php
+++ app/Widgets/News/views/index.blade.php
@@ -14,17 +14,43 @@
 		</{{ $params->get('item_heading', 'h3') }}>
 	<?php endif; ?>
 	<?php if (count($articles)): ?>
-		<ul class="newslist">
+		<ul class="newslist list-unstyled">
 			<?php foreach ($articles as $article): ?>
-				<li>
-					<a href="{{ route('site.news.show', ['id' => $article->id]) }}">{{ $article->headline }}</a>
-					<p class="date">{{ $article->formatDate($article->datetimenews->toDateTimeString(), $article->hasEnd() ? $article->datetimenewsend->toDateTimeString() : null) }}</p>
-					@if ($article->location)
-						<p class="date">{{ $article->location }}</p>
-					@endif
-					@if ($article->isUpdated() && $article->datetimeupdate != $article->datetimenewscreated)
-						<p class="newsupdated">{{ trans('widget.news::news.updated') }}: {{ $article->formatDate($article->datetimeupdate->toDateTimeString()) }}</p>
-					@endif
+				<li id="article-{{ $article->id }}" aria-labelledby="article-{{ $article->id }}-title" itemscope itemtype="https://schema.org/<?php echo ($type->calendar ? 'Event' : 'NewsArticle'); ?>">
+						<p id="article-{{ $article->id }}-title" class="news-title mb-0">
+							<a href="{{ route('site.news.show', ['id' => $article->id]) }}">{{ $article->headline }}</a>
+						</p>
+						<ul class="news-meta list-inline text-muted">
+							<li class="newsdate list-inline-item">
+								<span class="fa fa-fw fa-clock-o" aria-hidden="true"></span>
+								<time datetime="{{ $article->datetimenews->toDateTimeLocalString() }}">{{ $article->formatDate($article->datetimenews->toDateTimeString(), $article->hasEnd() ? $article->datetimenewsend->toDateTimeString() : null) }}</time>
+								@if ($article->isToday())
+									@if ($article->isNow())
+										<span class="badge badge-success">{{ trans('news::news.happening now') }}</span>
+									@else
+										<span class="badge badge-info">{{ trans('news::news.today') }}</span>
+									@endif
+								@elseif ($article->isTomorrow())
+									<span class="badge">{{ trans('news::news.tomorrow') }}</span>
+								@endif
+								@if ($article->isUpdated() && $article->datetimeupdate != $article->datetimenewscreated)
+									<span class="badge badge-warning">
+										<span class="fa fa-exclamation-circle" aria-hidden="true"></span>
+										{{ trans('widget.news::news.updated') }}: <time datetime="{{ $article->datetimeupdate->toDateTimeLocalString() }}">{{ $article->formatDate($article->datetimeupdate->toDateTimeString()) }}</time>
+									</span>
+								@endif
+							</li>
+							@if ($params->get('show_location') && $article->location)
+								<li class="newslocation list-inline-item">
+									{{ $article->location }}
+								</li>
+							@endif
+						</ul>
+						@if ($params->get('blurb_length', 150) > 0)
+							<p class="newsblurb" itemprop="description">
+								{{ Illuminate\Support\Str::limit(strip_tags($article->toHtml()), $params->get('blurb_length', 150)) }}
+							</p>
+						@endif
 				</li>
 			<?php endforeach; ?>
 		</ul>
