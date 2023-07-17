1a6cf3196940042708f6492f55180d6651ac4e74
Shawn Rice <zooley@purdue.edu>
2023-07-17 12:30:24 -0400

    fix: Remove extra 'c' in word

--- app/Listeners/Users/Request/lang/en/request.php
+++ app/Listeners/Users/Request/lang/en/request.php
@@ -2,5 +2,5 @@
 return [
 	'listener name' => 'Users - Request',
 	'listener desc' => 'This displays a page to request access to resources.',
-	'request access' => 'Request Acccess',
+	'request access' => 'Request Access',
 ];
