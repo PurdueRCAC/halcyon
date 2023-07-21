4acba7aded7dc6a652f55878ffd252aea38c5458
Shawn Rice <zooley@purdue.edu>
2023-07-21 10:17:58 -0400

    fix(search): Remove unused files

--- app/Modules/Search/Http/Resources/SnippetResourceCollection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Modules\Knowledge\Http\Resources;
-
-use Illuminate\Http\Resources\Json\ResourceCollection;
-
-class SnippetResourceCollection extends ResourceCollection
-{
-}
