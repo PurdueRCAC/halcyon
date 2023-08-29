61fddd2703c3d9ae4d3ae92b1e5b5b92cb48d457
Shawn Rice <zooley@purdue.edu>
2023-08-29 08:22:26 -0400

    fix: Use correct name, lang string, adjust style

--- app/Widgets/Search/views/index.blade.php
+++ app/Widgets/Search/views/index.blade.php
@@ -3,22 +3,25 @@
  * @package  Search widget
  */
 ?>
-@if (params->get('opensearch', 0))
+@if ($params->get('opensearch', 0))
 	@section('meta')
-		<link href="{{ url()->current() }}?format=opensearch" rel="search" type="application/opensearchdescription+xml" title="{{ $params->get('opensearch_title', trans('widget.search::search.button text') . ' ' . config('app.name')); }}" />
+		<link href="{{ url()->current() }}?format=opensearch" rel="search" type="application/opensearchdescription+xml" title="{{ $params->get('opensearch_title', trans('widget.search::search.button text') . ' ' . config('app.name')) }}" />
 	@stop
 @endif
 
-<form action="{{ route('site.pages.search') }}" method="get" id="searchform{{ $instance }}" class="{{ $class }} searchform">
+<form action="{{ route('site.search.index') }}" method="get" id="searchform{{ $instance }}" class="{{ $class }} searchform">
 	<fieldset>
 		<legend>{{ $text }}</legend>
 
 		<?php
 		$output  = '<label for="searchword' . $instance . '" class="' . $class . 'searchword-label" id="searchword-label' . $instance . '">' . $label . '</label>';
-		$output .= '<input type="search" enterkeyhint="search" name="terms" class="' . $class . 'searchword" id="searchword' . $instance . '" size="' . $width . '" placeholder="' . $text . '" />';
+		if ($button && ($button_pos == 'right' || $button_pos == 'left')):
+			$output .= '<span class="input-group">';
+		endif;
+		$output .= '<input type="search" enterkeyhint="search" name="search" class="form-control ' . $class . 'searchword" id="searchword' . $instance . '" size="' . $width . '" placeholder="' . $text . '" />';
 
 		if ($button):
-			$button = '<input type="submit" class="' . $class . 'searchsubmit" id="submitquery' . $instance . '" value="' . $button_text . '" />';
+			$button = '<input type="submit" class="' . $class . 'searchsubmit input-group-text" id="submitquery' . $instance . '" value="' . $button_text . '" />';
 		endif;
 
 		switch ($button_pos):
@@ -31,14 +34,18 @@
 			break;
 
 			case 'right':
-				$output = $output . $button;
+				$output = $output . '<span class="input-group-append">' . $button . '</span>';
 			break;
 
 			case 'left':
 			default:
-				$output = $button . $output;
+				$output = '<span class="input-group-prepend">' . $button . '</span>' . $output;
 			break;
 		endswitch;
+
+		if ($button && ($button_pos == 'right' || $button_pos == 'left')):
+			$output .= '</span>';
+		endif;
 
 		echo $output;
 		?>
