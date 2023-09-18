cb0de30cf0d5d5635fa663fc49c22bbb20be47dc
Shawn Rice <zooley@purdue.edu>
2023-09-18 14:08:05 -0400

    feat(history): Can register formatters for output This allows registering formatters for transforming an entry into a human readable format

--- app/Modules/History/Models/History.php
+++ app/Modules/History/Models/History.php
@@ -67,6 +67,15 @@ class History extends Model
 	public static $orderDir = 'desc';
 
 	/**
+	 * Processors that will process all log records
+	 *
+	 * To process records of a single handler instead, add the processor on that specific handler
+	 *
+	 * @var callable[]
+	 */
+	protected static $processors = array();
+
+	/**
 	 * Historable relationship
 	 *
 	 * @return  MorphTo
@@ -117,6 +126,28 @@ class History extends Model
 	}
 
 	/**
+	 * Get actor's name
+	 *
+	 * @return  string
+	 */
+	public function getActorAttribute(): ?string
+	{
+		$actor = trans('global.unknown');
+
+		if (!$this->user_id)
+		{
+			$actor = trans('global.system user');
+		}
+
+		if ($this->user)
+		{
+			$actor = e($this->user->name);
+		}
+
+		return $actor;
+	}
+
+	/**
 	 * Get a diff of the specified column
 	 *
 	 * @param   string  $column
@@ -145,5 +176,64 @@ class History extends Model
 		$formatter = new Table();
 
 		return $formatter->format(new Diff($ota, $nta));
+	}
+
+	/**
+	 * Adds a processor on to the stack.
+	 *
+	 * @param callable $callback
+	 * @return void
+	 */
+	public static function pushProcessor(callable $callback)
+	{
+		array_unshift(self::$processors, $callback);
+	}
+
+	/**
+	 * Removes the processor on top of the stack and returns it.
+	 *
+	 * @throws \LogicException If empty processor stack
+	 * @return callable
+	 */
+	public static function popProcessor(): callable
+	{
+		if (!self::$processors)
+		{
+			throw new \LogicException('You tried to pop from an empty processor stack.');
+		}
+
+		return array_shift(self::$processors);
+	}
+
+	/**
+	 * Get the lsit of processors
+	 *
+	 * @return callable[]
+	 */
+	public static function getProcessors(): array
+	{
+		return self::$processors;
+	}
+
+	/**
+	 * Process a History entry
+	 *
+	 * @param  History $record
+	 * @param  Model|null $model
+	 * @return History
+	 */
+	public function process($record, Model $model = null): History
+	{
+		foreach (self::$processors as $processor)
+		{
+			$record = $processor($record, $model);
+
+			if ($record->summary)
+			{
+				break;
+			}
+		}
+
+		return $record;
 	}
 }
