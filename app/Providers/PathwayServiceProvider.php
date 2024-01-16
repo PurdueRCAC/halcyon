9c8252e5defd985873906a5901ea70f1d6b9e685
Shawn Rice <zooley@purdue.edu>
2024-01-16 10:21:29 -0500

    chore: Remove unnecssary (and mostly unused) code

--- app/Providers/PathwayServiceProvider.php
+++ app/Providers/PathwayServiceProvider.php
@@ -18,64 +18,7 @@ class PathwayServiceProvider extends ServiceProvider
 	{
 		$this->app->singleton('pathway', function ($app)
 		{
-			$pathway = new Trail();
-
-			if ($app->has('menu'))
-			{
-				$menu = $app['menu'];
-
-				if ($item = $menu->getActive())
-				{
-					$menus = $menu->getMenu();
-					$home  = $menu->getDefault();
-
-					if ($home && $item->id != $home->id)
-					{
-						foreach ($item->tree as $menupath)
-						{
-							$url = '';
-							$link = $menu->getItem($menupath);
-
-							if (!$link)
-							{
-								continue;
-							}
-
-							switch ($link->type)
-							{
-								case 'separator':
-									$url = null;
-									break;
-
-								case 'url':
-									if ((strpos($link->link, 'index.php?') === 0) && (strpos($link->link, 'Itemid=') === false))
-									{
-										// If this is an internal link, ensure the Itemid is set.
-										$url = $link->link . '&Itemid=' . $link->id;
-									}
-									else
-									{
-										$url = $link->link;
-									}
-									break;
-
-								case 'alias':
-									// If this is an alias use the item id stored in the parameters to make the link.
-									$url = 'index.php?Itemid=' . $link->params->get('aliasoptions');
-									break;
-
-								default:
-									$url = $link->link;
-									break;
-							}
-
-							$pathway->append($menus[$menupath]->title, $url);
-						}
-					}
-				}
-			}
-
-			return $pathway;
+			return new Trail();
 		});
 	}
 }
