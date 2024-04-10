7552b7e55e231665745c2c072b4e44282ac4daeb
Shawn Rice <zooley@purdue.edu>
2024-04-10 13:23:08 -0400

    fix: Replace dup config opt with needed opt

--- config/sanctum.php
+++ config/sanctum.php
@@ -75,7 +75,7 @@ return [
     */
 
     'middleware' => [
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
+        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
         'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
     ],
