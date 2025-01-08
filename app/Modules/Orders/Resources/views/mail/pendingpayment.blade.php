26f6b8b9baa242ac1990ad9c03a46f29180fad10
ckabuo <ckabuo@purdue.edu>
2025-01-08 12:21:52 -0500

    Updated pending payment email messages

--- app/Modules/Orders/Resources/views/mail/pendingpayment.blade.php
+++ app/Modules/Orders/Resources/views/mail/pendingpayment.blade.php
@@ -4,9 +4,15 @@ Hello {{ $user->name }},
 @if ($order->total > 0)
 This is an automated message notifying that a {{ $order->type }} order has been entered and is awaiting payment information so it may be processed.
 
-Please review your order and enter the account numbers to be used for payment on the [Order Management]({{ route('site.orders.read', ['id' => $order->id]) }}) page. You may edit or cancel the order before entering account information. Once payment information is entered, your order will be sent to the appropriate business office(s) for funding approval.
+Please review your order and, if needed adjust the account numbers to be used for payment on the [Order Management]({{ route('site.orders.read', ['id' => $order->id]) }}) page. 
+If you need to edit or cancel the order, please reach out to us at [support](rcac-help@purdue.edu).
 @else
-This is an automated message notifying that a {{ $order->type }} order has been entered and is awaiting processing. You will receive another email once the order is fulfilled.
+This is an automated message notifying that a {{ $order->type }} order has been entered and is awaiting payment information so it may be processed.
+
+Please review your order and enter the account numbers to be used for payment on the [Order Management]({{ route('site.orders.read', ['id' => $order->id]) }}) page. You may edit or cancel the order before entering account information.
+Once payment information is entered, your order will be sent to the appropriate business office(s) for funding approval.
+
+You will receive another email once the order is fulfilled.
 @endif
 
 ---
